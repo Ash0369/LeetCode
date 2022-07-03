ca04d0dc16a787999ccacd004db9c6d79c7ec56f
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-03 16:36:31 +0530

    Create 222.cpp

---
+++ 222.cpp
@@ -0,0 +1,43 @@
+//Method-1
+
+int sum(TreeNode* root)
+{
+    int c=0;
+    if(root==NULL)
+    {
+        return -1;
+    }
+    c=1+sum(root->left);
+    c=c+1+sum(root->right);
+    return c;
+}
+class Solution
+{
+public:
+    int countNodes(TreeNode* root) 
+    {
+        return sum(root)+1;
+    }
+};
+
+//Method-2
+
+int count(TreeNode* root)
+{
+    int x,y;
+    if(root!=NULL)
+    {
+        x=count(root->left);
+        y=count(root->right);
+        return x+y+1;
+    }
+    return 0;
+}
+class Solution
+{
+public:
+    int countNodes(TreeNode* root) 
+    {
+        return count(root);
+    }
+};

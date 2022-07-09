ffb6c4e0cbf64a94a62e1c512b48a3ca53f552b4
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-09 19:00:48 +0530

    Update 230.cpp

--- 230.cpp
+++ 230.cpp
@@ -1,3 +1,5 @@
+//Method-1 : O(n) Space Complexity
+
 vector<int>preorder;
 void preorder_traversal(TreeNode* root)
 {
@@ -18,5 +20,37 @@ public:
         preorder.clear();
         preorder_traversal(root);
         return preorder[k-1];
+    }
+};
+
+//Method-2 : O(1) Space Complexity.
+
+int counter=0;
+int result;
+void preorder_traversal(TreeNode* root,int k)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    
+    preorder_traversal(root->left,k);
+    counter++;
+    if(counter==k)
+    {
+        result=root->val;
+    }
+    preorder_traversal(root->right,k);
+}
+class Solution 
+{
+public:
+    int kthSmallest(TreeNode* root, int k) 
+    {
+        //preorder traversal is sorted of bst
+        counter=0;
+        result=0;
+        preorder_traversal(root,k);
+        return result;
     }
 };

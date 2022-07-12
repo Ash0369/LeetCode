00addb1d7e77b8d37140b2248574df098103e54f
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-12 06:12:13 +0530

    Create 99.cpp

---
+++ 99.cpp
@@ -0,0 +1,45 @@
+//Method-1 : O(n) Space Complexity and O(n*log(n)) Time Complexity
+
+vector<int>inorder;
+void make_inorder(TreeNode* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    make_inorder(root->left);
+    inorder.push_back(root->val);
+    make_inorder(root->right);
+}
+int i=0;
+void make_tree(TreeNode* &root)
+{
+   if(root==NULL)
+   {
+        return;
+   }
+   make_tree(root->left);
+   if(inorder[i]==root->val)
+   {
+
+   }
+   else
+   {
+        root->val=inorder[i];
+   }
+   i++;
+   make_tree(root->right);
+}
+class Solution 
+{
+public:
+    void recoverTree(TreeNode* root) 
+    {
+        inorder.clear();
+        make_inorder(root);
+        sort(inorder.begin(),inorder.end());
+        i=0;
+        make_tree(root);
+
+    }
+};

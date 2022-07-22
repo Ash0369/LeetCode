7581f4179223bf6adfe810555868f485f8e83ebb
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-22 22:41:48 +0530

    Update 785.cpp

--- 785.cpp
+++ 785.cpp
@@ -49,3 +49,49 @@ public:
         return true;
     }
 };
+
+//Method-2: DFS
+
+class Solution 
+{
+public:
+    bool make_color(int node,int parent,vector<vector<int>>& graph,vector<int>&color)
+    {
+        color[node]=1-color[parent];  
+        for(auto it:graph[node])
+        {
+            if(color[it]==-1)
+            {
+                if(!make_color(it,node,graph,color))
+                {
+                    return false;
+                }
+            }
+            else if(color[it]==color[node])
+            {
+                return false;
+            }
+        }
+        return true; 
+    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n=graph.size();
+        vector<int>color(n,0);
+        for(int i=0;i<n;i++)
+        {
+            color[i]=-1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(!make_color(i,0,graph,color))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};

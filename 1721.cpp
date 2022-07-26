3b3fb0407375cec8fd365a737a5f5dac57f55894
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-26 13:27:55 +0530

    Update 1721.cpp

--- 1721.cpp
+++ 1721.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Time Complexity O(n) and Space Complexity O(n)
+
 void swap(ListNode* a,ListNode* b)
 {
     int c=a->val;
@@ -17,6 +19,36 @@ public:
             temp=temp->next;
         }
         swap(vec[k-1],vec[vec.size()-k]);
+        return head;
+    }
+};
+
+//Method-2 : Two Pointers,Time Complexity O(n) and Space Complexity O(1)
+
+class Solution 
+{
+public:
+    ListNode* swapNodes(ListNode* head, int k) 
+    {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* first=head;
+        while(k>1)
+        {
+            fast=fast->next;
+            first=first->next;
+            k--;
+        }
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+
+        int c;
+        c=first->val;
+        first->val=slow->val;
+        slow->val=c;
         return head;
     }
 };

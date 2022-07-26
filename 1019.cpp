4ce7c4574301b6f0b52294cf6da9a303d31891a4
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-26 23:02:43 +0530

    Update 1019.cpp

--- 1019.cpp
+++ 1019.cpp
@@ -27,3 +27,43 @@ public:
         return result;
     }
 };
+
+//Method-2 : Using Satck
+
+class Solution 
+{
+public:
+    vector<int> nextLargerNodes(ListNode* head) 
+    {
+        vector<int>result;
+        stack<int>curr;
+        stack<int>past;
+        while(head!=NULL)
+        {
+            curr.push(head->val);
+            head=head->next;
+        }
+        past.push(curr.top());
+        curr.pop();
+        result.push_back(0);
+        while(!curr.empty())
+        {
+            while(!past.empty() && curr.top()>=past.top())
+            {
+                past.pop();
+            }
+            if(past.empty())
+            {
+                result.push_back(0);
+            }
+            else
+            {
+                result.push_back(past.top());
+            }
+            past.push(curr.top());
+            curr.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};

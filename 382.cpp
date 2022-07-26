8fb7cbec3b0f8bfcc630b1cea2e07a6a7764a4e2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-27 04:57:19 +0530

    Update 382.cpp

--- 382.cpp
+++ 382.cpp
@@ -1,4 +1,4 @@
-//Space Complexity O(n)
+//Method-1 : Space Complexity O(n)
 
 class Solution 
 {
@@ -20,5 +20,35 @@ public:
         int n=result.size();
         int random_number=(rand()%n);
         return result[random_number];
+    }
+};
+
+//Method-2 : Space Complexity O(1)
+
+class Solution 
+{
+public:
+    int n=0;
+    ListNode* per_head;
+    Solution(ListNode* head) 
+    {
+        per_head=head;
+        while(head!=NULL)
+        {
+            n++;
+            head=head->next;
+        }
+    }
+    
+    int getRandom() 
+    {
+        int random_number=rand()%n;
+        ListNode* temp=per_head;
+        while(random_number>0)
+        {
+            temp=temp->next;
+            random_number--;
+        }
+        return temp->val;
     }
 };

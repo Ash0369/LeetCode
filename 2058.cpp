4f282c8ec8bb72dacf3c110ed3c518b57cd5abb5
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-27 23:07:39 +0530

    Update 2058.cpp

--- 2058.cpp
+++ 2058.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Using Some extra vector
+
 class Solution 
 {
 public:
@@ -39,6 +41,62 @@ public:
         }
         result.push_back(min_val);
         result.push_back(pos[pos.size()-1]-pos[0]);
+        return result;
+    }
+};
+
+
+//Method-2 : Reducing space used
+
+class Solution 
+{
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) 
+    {
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* aftr=NULL;
+        int min_value=INT_MAX;
+        int max_value=INT_MIN;
+        int last_index=-1;
+        int first_index=-1;
+        int count=0;
+        while(curr!=NULL)
+        {
+            aftr=curr->next;
+            if(prev!=NULL && aftr!=NULL)
+            {
+                if((curr->val>prev->val && curr->val>aftr->val) || (curr->val<prev-> val  && curr->val<aftr->val))
+                {
+                    if(last_index==-1)
+                    {
+                        last_index=count;
+                        first_index=count;
+                    }
+                    else
+                    {
+                        max_value=max(max_value,abs(first_index-count));
+
+                        min_value=min(min_value,abs(last_index-count));
+
+                        last_index=count;
+                    }
+
+                }
+            }
+            count++;
+            prev=curr;
+            curr=curr->next;
+        }
+        vector<int>result;
+        if(last_index==-1 || max_value==INT_MIN)
+        {
+            result.push_back(-1);
+            result.push_back(-1);
+            return result;
+        }
+        result.push_back(min_value);
+        result.push_back(max_value);
         return result;
     }
 };

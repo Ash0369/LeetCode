fa5a4f5bc7ab66a1ba3493ec0d2604fa37b87f51
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-27 22:25:20 +0530

    Update 641.cpp

--- 641.cpp
+++ 641.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Using Dequeue
+
 class MyCircularDeque 
 {
 public:
@@ -78,6 +80,144 @@ public:
     bool isFull() 
     {
         if(dq.size()==size)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+
+//Method-2 : Using Doubly Linked List
+
+struct List_Node {
+    int val;
+    List_Node *next;
+    List_Node *prev;
+    List_Node() : val(0), next(nullptr),prev(nullptr) {}
+    List_Node(int x) : val(x), next(nullptr),prev(nullptr) {}
+    List_Node(int x, List_Node *next,List_Node*prev) : val(x), next(next),prev(prev) {}
+};
+
+class MyCircularDeque 
+{
+public:
+
+    List_Node* head=new List_Node(0);
+    List_Node* tail=new List_Node(0);
+    int size=0;
+    int max_size;
+    MyCircularDeque(int k) 
+    {
+        max_size=k;   
+    }
+    
+    bool insertFront(int value) 
+    {
+        if(isFull())
+        {
+            return false;
+        }
+        List_Node* temp=new List_Node(value);
+        if(isEmpty())
+        {
+            head=temp;
+            tail=temp;
+            head->prev=NULL;
+            tail->prev=NULL;
+        }
+        else
+        {
+            temp->next=head;
+            head->prev=temp;
+            head=temp;
+        }
+        size++;
+        return true;
+    }
+    
+    bool insertLast(int value) 
+    {
+        if(isFull())
+        {
+            return false;
+        }
+        List_Node* temp=new List_Node(value);
+
+        if(isEmpty())
+        {
+            head=temp;
+            tail=temp;
+            head->prev=NULL;
+            tail->prev=NULL;
+        }
+        else
+        {
+            temp->prev=tail;
+            tail->next=temp;
+            tail=temp;
+        }
+        size++;
+        return true;
+
+    }
+    
+    bool deleteFront() 
+    {
+        if(isEmpty())
+        {
+            return false;
+        }
+        List_Node* todel=head;
+        head=head->next;
+        delete(todel);
+        size--;
+        return true;
+    }
+    
+    bool deleteLast() 
+    {
+        if(isEmpty())
+        {
+            return false;
+        }
+        List_Node* todel=tail;
+        tail=tail->prev;
+        delete(todel);
+        size--;
+        return true;
+    }
+    
+    int getFront() 
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return head->val;
+    }
+    
+    int getRear() 
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return tail->val;
+    }
+    
+    bool isEmpty() 
+    {
+        if(size==0)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    bool isFull() 
+    {
+        if(size==max_size)
         {
             return true;
         }

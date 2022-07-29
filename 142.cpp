68b919002c2f02560e9ae052409c9f242f5f76ff
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-29 16:17:13 +0530

    Update 142.cpp

--- 142.cpp
+++ 142.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Brute Force
+
 class Solution 
 {
 public:
@@ -13,6 +15,37 @@ public:
             }
             head=head->next;
         }
+        return NULL;
+    }
+};
+
+
+//Method-2 : Floyd's Algorthim
+
+class Solution 
+{
+public:
+    ListNode *detectCycle(ListNode *head) 
+    {
+        
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+            {
+                fast=head;
+                while(fast!=slow)
+                {
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return fast;
+            }
+        }
+        
         return NULL;
     }
 };

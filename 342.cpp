fbbec7ea09c9dc15ca1d1cf8dd9a33d726bbe492
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-30 21:16:30 +0530

    Update 342.cpp

--- 342.cpp
+++ 342.cpp
@@ -32,10 +32,10 @@ public:
 class Solution 
 {
 public:
-    bool isPowerOfTwo(int n) 
+    bool isPowerOfFour(int n) 
     {
         long long int x=1;
-        for(int i=0;i<=31;i++)
+        for(int i=0;i<=31;i=i+2)
         {
             x=1<<i;
             if(x>n)

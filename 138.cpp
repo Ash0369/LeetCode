156aba0f4df3999c892c39408069be461181285a
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-30 05:53:33 +0530

    Update 138.cpp

--- 138.cpp
+++ 138.cpp
@@ -24,3 +24,55 @@ public:
         return copy(head,mp);
     }
 };
+
+//Method-2 : Optimized Approach
+
+class Solution 
+{
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        Node* temp=head;
+
+        //First making LL
+
+        while(temp!=NULL)
+        {
+            Node* new_node=new Node(temp->val);
+            Node* curr=temp;
+            temp=temp->next;
+            curr->next=new_node;
+            new_node->next=temp;
+        }
+
+        //Now connect random
+
+        temp=head;
+        while(temp!=NULL)
+        {
+            if(temp->random!=NULL)
+            {
+                temp->next->random=temp->random->next;
+            }
+            temp=temp->next->next;
+        }
+
+        //Removing older item
+
+        temp=head;
+        Node* dummy=new Node(0);
+        Node* copy,*copy_tail=dummy;
+
+        while(temp!=NULL)
+        {
+            Node* nxt=temp->next->next;
+            copy=temp->next;
+            copy_tail->next=copy;
+            copy_tail=copy;
+
+            temp->next=nxt;
+            temp=nxt;
+        }
+         return dummy->next;
+    }
+};

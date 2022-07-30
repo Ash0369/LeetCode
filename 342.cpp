b9c84c0627bc61009fa4180c6a0f11ccf9e5f1bc
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-30 21:26:41 +0530

    Update 342.cpp

--- 342.cpp
+++ 342.cpp
@@ -50,3 +50,22 @@ public:
         return false;
     }
 };
+
+//Method-3 : Without Loop
+
+class Solution 
+{
+public:
+    bool isPowerOfFour(int n) 
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        if((n&(n-1))==0  && (n-1)%3==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};

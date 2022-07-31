93ed7eb099c8e8169a1a322c9ad79152b0fa5c42
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-01 05:17:02 +0530

    Create 46.cpp

---
+++ 46.cpp
@@ -0,0 +1,32 @@
+vector<vector<int>>result;
+void permutation(vector<int>&nums,vector<int>&per,vector<int>&visited)
+{
+    if(per.size()==nums.size())
+    {
+        result.push_back(per);
+        return;
+    }
+    for(int i=0;i<visited.size();i++)
+    {
+        if(visited[i]==0)
+        {
+            visited[i]=1;
+            per.push_back(nums[i]);
+            permutation(nums,per,visited);
+            visited[i]=0;
+            per.pop_back();
+        }
+    }
+}
+class Solution 
+{
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        result.clear();
+        vector<int>visited(nums.size(),0);
+        vector<int>per;
+        permutation(nums,per,visited);
+        return result;
+    }
+};

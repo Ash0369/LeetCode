66553c21acc6f046595dd0991b70adfb106b6f46
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-07-31 10:22:39 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,35 @@
+vector<vector<int>>result;
+void combiantion(vector<int>&candidates,int target,vector<int>cs,int sum,int index)
+{
+    if(index>=candidates.size() && sum!=target)
+    {
+        return;
+    }
+    if(sum==target)
+    {
+        result.push_back(cs);
+        return;
+    }
+    int a=(target-sum)/candidates[index];
+    for(int i=1;i<=a;i++)
+    {
+        cs.push_back(candidates[index]);
+        combiantion(candidates,target,cs,sum+(i*candidates[index]),index+1);
+    }
+    for(int i=1;i<=a;i++)
+    {
+        cs.pop_back();
+    }
+    combiantion(candidates,target,cs,sum,index+1);
+}
+class Solution 
+{
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        result.clear();
+        vector<int>cs;
+        combiantion(candidates,target,cs,0,0);
+        return result;
+    }
+};

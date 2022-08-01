e53d63de5f6d9f7e49c6bd6c6d5eb327f362840b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-01 21:33:31 +0530

    Create 22.cpp

---
+++ 22.cpp
@@ -0,0 +1,29 @@
+vector<string>result;
+void generate(int open,int close,string s,int n)
+{
+    if(s.length()==(2*n))
+    {
+        result.push_back(s);
+    }
+    if(open<n)
+    {
+        s.push_back('(');
+        generate(open+1,close,s,n);
+        s.pop_back();
+    }
+    if(close<n && close<open)
+    {
+        s.push_back(')');
+        generate(open,close+1,s,n);
+        s.pop_back();
+    }
+}
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) 
+    {
+        result.clear();
+        generate(1,0,"(",n);
+        return result;
+    }
+};

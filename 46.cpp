c08c2daa39e1e96fc1d504e80eec43f073193305
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-01 07:13:02 +0530

    Update 46.cpp

--- 46.cpp
+++ 46.cpp
@@ -1,4 +1,4 @@
-//Time Complexity : O(n!*n) and Space Complexity : O(n)+O(n)
+//Method1- Time Complexity : O(n!*n) and Space Complexity : O(n)+O(n) , Auxillary Space O(n)
 
 vector<vector<int>>result;
 void permutation(vector<int>&nums,vector<int>&per,vector<int>&visited)
@@ -29,6 +29,41 @@ public:
         vector<int>visited(nums.size(),0);
         vector<int>per;
         permutation(nums,per,visited);
+        return result;
+    }
+};
+
+//Method-2 : Time Complexity O(n!*n) , Space Complexity O(1) , Auxillary Space O(n)
+
+vector<vector<int>>result;
+void swap(int &a,int &b)
+{
+    int c=a;
+    a=b;
+    b=c;
+    return;
+}
+void permutation(vector<int>&nums,int index)
+{
+    if(index==(nums.size()))
+    {
+        result.push_back(nums);
+        return;
+    }
+    for(int i=index;i<nums.size();i++)
+    {
+        swap(nums[index],nums[i]);
+        permutation(nums,index+1);
+        swap(nums[index],nums[i]);
+    }
+}
+class Solution 
+{
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        result.clear();
+        permutation(nums,0);
         return result;
     }
 };

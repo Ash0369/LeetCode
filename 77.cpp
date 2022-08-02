8c56b04f91f57a794e268043603ba3890ee338d7
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-02 05:51:52 +0530

    Create 77.cpp

---
+++ 77.cpp
@@ -0,0 +1,37 @@
+//Method-1 : First creating an array and finding it's subsequence of length==k 
+
+
+vector<vector<int>>result;
+void combination(vector<int>&curr,int k,vector<int>&nums,int index)
+{
+    if(curr.size()==k)
+    {
+        result.push_back(curr);
+        return;
+    }
+    if(nums.size()<=index  || curr.size()>k)
+    {
+        return;
+    }
+    combination(curr,k,nums,index+1);
+    curr.push_back(nums[index]);
+    combination(curr,k,nums,index+1);
+    curr.pop_back();
+    
+}
+class Solution 
+{
+public:
+    vector<vector<int>>combine(int n, int k) 
+    {
+        result.clear();
+        vector<int>curr;
+        for(int i=1;i<=n;i++)
+        {
+           curr.push_back(i);
+        }
+        vector<int>temp;
+        combination(temp,k,curr,0);
+        return result;
+    }
+};

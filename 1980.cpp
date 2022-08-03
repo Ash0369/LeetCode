4765bec6fb446f493b0d36df934c6ef506a13630
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-03 14:50:58 +0530

    Update 1980.cpp

--- 1980.cpp
+++ 1980.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Brute Force,15ms
+
 string s;
 void find_different(map<string,int>&mp,string formed,int n)
 {
@@ -15,6 +17,46 @@ void find_different(map<string,int>&mp,string formed,int n)
             formed[i]='0';
         }  
     }
+}
+class Solution 
+{
+public:
+    string findDifferentBinaryString(vector<string>&nums) 
+    {
+        map<string,int>mp;
+        string formed;
+        for(int i=0;i<nums.size();i++)
+        {
+            formed.push_back('0');
+            mp[nums[i]]++;
+        }
+        find_different(mp,formed,nums.size());
+        return s;
+    }
+};
+
+
+//Method-2 : Optimized Brute Force ,10ms
+
+string s;
+bool find_different(map<string,int>&mp,string &formed,int n)
+{
+    if(mp.find(formed)==mp.end())
+    {
+        s=formed;
+        return true;
+    }
+    bool res=false;
+    for(int i=0;i<n && res==false;i++)
+    {
+        if(formed[i]=='0')
+        {
+            formed[i]='1'; //Turning on ith bit and calling recurssion
+            res=find_different(mp,formed,n);
+            formed[i]='0';
+        }  
+    }
+    return res;
 }
 class Solution 
 {

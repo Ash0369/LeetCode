e2c723831ed1a077ab248f639d3751bec3cfa657
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-05 05:24:57 +0530

    Update 70.cpp

--- 70.cpp
+++ 70.cpp
@@ -32,3 +32,25 @@ public:
         return climb(n,0,vec);
     }
 };
+
+//Method-2 : Bottom Up Approach
+
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        vector<int>ways(n+1,0);
+        ways[1]=1; //No of ways to reach 1st stair
+        ways[2]=2; //No of ways to reach 2nd stair
+        for(int i=3;i<=n;i++)
+        {
+            ways[i]=ways[i-1]+ways[i-2];
+        }
+        return ways[n];
+    }
+};

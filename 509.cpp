be32fef9fae8dc3f08137ab90a01a05e7686f9fd
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-04 05:31:18 +0530

    Update 509.cpp

--- 509.cpp
+++ 509.cpp
@@ -1,3 +1,5 @@
+//Metod-1 : Brute Force
+
 class Solution 
 {
 public:
@@ -12,5 +14,40 @@ public:
             return 1;
         }
         return fib(n-1)+fib(n-2);
+    }
+};
+
+
+//Method-2 : DP
+
+int fibonacci(int n,vector<int>&vec)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    if(n<=0)
+    {
+        return 0;
+    }
+    if(vec[n-2]==-1)
+    {
+        vec[n-2]=fibonacci(n-2,vec);
+    }
+    if(vec[n-1]==-1)
+    {
+        vec[n-1]=fibonacci(n-1,vec);
+    }
+     vec[n]=vec[n-1]+vec[n-2];
+    return vec[n];
+
+}
+class Solution 
+{
+public:
+    int fib(int n) 
+    {
+        vector<int>vec(n+1,-1);
+        return fibonacci(n,vec);
     }
 };

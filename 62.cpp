7f4f2cebc657612c071a2536c58947dd0187f456
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-11 15:24:06 +0530

    Update 62.cpp

--- 62.cpp
+++ 62.cpp
@@ -1,4 +1,4 @@
-//Method-1 : Time Complexity 2^(m*n)
+//Method-1 : Time Complexity O(m*n) and Space Complexity O(m*n) + O(m+n)
 
 
 int  count_paths(int x,int y,vector<vector<int>>&vec)
@@ -26,5 +26,37 @@ public:
     {
         vector<vector<int>>vec(m,vector<int>(n,-1));
         return count_paths(m-1,n-1,vec);
+    }
+};
+
+//Method-2 : Tabulation : Time Complexity O(m*n) and Space Complexity O(m*n)
+
+class Solution 
+{
+public:
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>>vec(m,vector<int>(n,-1));
+        vec[0][0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j!=0)
+                {
+                    vec[i][j]=vec[i][j-1];
+                }
+                else if(i!=0 && j==0)
+                {
+                    vec[i][j]=vec[i-1][j];
+                }
+                else if(i!=0 && j!=0)
+                {
+                    vec[i][j]=vec[i-1][j]+vec[i][j-1];
+                }
+            }
+        }
+        return vec[m-1][n-1];
+
     }
 };

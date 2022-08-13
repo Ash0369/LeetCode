ad37778160429b2f3b82103fd0fe1a76e5dcab67
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-13 15:50:21 +0530

    Update 1463.cpp

--- 1463.cpp
+++ 1463.cpp
@@ -99,3 +99,65 @@ public:
         return cp(grid,0,0,grid[0].size()-1,dp);
     }
 };
+
+
+//Method-3 : Tabulation 
+
+
+class Solution 
+{
+public:
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int r=grid.size();
+        int c=grid[0].size();
+        vector<vector<vector<int>>>dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
+        for(int j1=0;j1<c;j1++)
+        {
+            for(int j2=0;j2<c;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[r-1][j1][j2]=grid[r-1][j1];
+                }
+                else
+                {
+                    dp[r-1][j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+                }
+            }
+        }
+        for(int i=r-2;i>=0;i--)
+        {
+            for(int j1=0;j1<c;j1++)
+            {
+                for(int j2=0;j2<c;j2++)
+                {
+                    int nxt_lvl=-1e8;
+                    for(int a=-1;a<=1;a++)
+                    {
+                        for(int b=-1;b<=1;b++)
+                        {
+                            int cherry=-1e8;
+                            if(j1+a>=0 && j2+b>=0 && j1+a<c && j2+b<c)
+                            {
+                                if(j1==j2)
+                                {
+                                    cherry=grid[i][j1];
+                                }
+                                else
+                                {
+                                    cherry=grid[i][j2]+grid[i][j1];
+                                }
+                                cherry=cherry+dp[i+1][j1+a][j2+b];
+                            }
+                            nxt_lvl=max(nxt_lvl,cherry);
+                        }
+                    }
+                    dp[i][j1][j2]=nxt_lvl;
+                }
+            }
+        }
+        return dp[0][0][c-1];
+        
+    }
+};

6697fa17fe46ebf2fd65bce138533c001697192c
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-13 15:23:53 +0530

    Update 1463.cpp

--- 1463.cpp
+++ 1463.cpp
@@ -46,3 +46,56 @@ public:
         return cp(grid,0,0,grid[0].size()-1);
     }
 };
+
+//Method-2 : Memoization Time Complexity O(r*c*c) and Space Complexity O(r)+O(r*c*c)
+
+int cp(vector<vector<int>>&grid,int i,int j1,int j2,vector<vector<vector<int>>>&dp)
+{
+    if(j1<0 || j2<0 || j1>=grid[0].size() || j2>=grid[0].size())
+    {
+        return -1e8;
+    }
+    if(i==grid.size()-1)
+    {
+        if(j1==j2)
+        {
+            return grid[i][j1];
+        }
+        return grid[i][j1]+grid[i][j2];
+    }
+
+    if(dp[i][j1][j2]==-1)
+    {
+        int nxt_lvl=-1e8;
+        for(int a=-1;a<=1;a++)
+        {
+            for(int b=-1;b<=1;b++)
+            {
+                int cherry=0;
+                if(j1==j2)
+                {
+                    cherry=grid[i][j1];
+                }
+                else
+                {
+                    cherry=grid[i][j2]+grid[i][j1];
+                }
+                cherry=cherry+cp(grid,i+1,j1+a,j2+b,dp);
+                nxt_lvl=max(nxt_lvl,cherry);
+            }
+        }
+        dp[i][j1][j2]=nxt_lvl;
+    }
+    return dp[i][j1][j2];
+}
+class Solution 
+{
+public:
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int r=grid.size();
+        int c=grid[0].size();
+        vector<vector<vector<int>>>dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
+        return cp(grid,0,0,grid[0].size()-1,dp);
+    }
+};

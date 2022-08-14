d44045d56368ee58786e86fedb66f1d9280b96d1
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-14 13:44:48 +0530

    Update 416.cpp

--- 416.cpp
+++ 416.cpp
@@ -45,3 +45,50 @@ public:
         return res(nums,nums.size()-1,k,dp);
     }
 };
+
+
+//Method-2 : Tabulation
+
+class Solution 
+{
+public:
+    bool canPartition(vector<int>& nums) 
+    {
+        int sum=0;
+        for(auto x:nums)
+        {
+            sum=sum+x;
+        }
+        if(sum%2!=0)
+        {
+            return false;
+        }
+        int k=sum/2;
+        vector<vector<bool>>dp(nums.size(),vector<bool>(k+1,false));
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=true;
+        }
+
+        if(nums[0]<=k)
+        {
+            dp[0][nums[0]]=true;
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=k;j++)
+            {
+                bool not_taking=dp[i-1][j];
+                bool taking=false;
+                if(j>=nums[i])
+                {
+                   taking= dp[i-1][j-nums[i]];
+                }
+                dp[i][j]= not_taking|taking;
+            }
+        }
+        return dp[n-1][k];
+    }
+};

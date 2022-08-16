3a4da0e475c8fbb14e2579166368248e53d4dc4d
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-16 12:19:17 +0530

    Update 94.cpp

--- 94.cpp
+++ 94.cpp
@@ -48,3 +48,50 @@ public:
         return result;
     }
 };
+
+
+/Method-3 : Using Visited Array
+
+class Solution 
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return {};
+        }
+        stack<TreeNode*>st;
+        vector<int>result;
+        set<TreeNode*>vis;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode* temp=st.top();
+            st.pop();
+
+            if(temp->left==NULL && temp->right==NULL)
+            {
+                result.push_back(temp->val);
+                continue;
+            }
+            if(vis.find(temp)!=vis.end())
+            {
+                result.push_back(temp->val);
+                continue;
+            }
+
+            if(temp->right!=NULL)
+            {
+                st.push(temp->right);
+            }
+            st.push(temp);
+            if(temp->left!=NULL)
+            {
+                st.push(temp->left);
+            }
+            vis.insert(temp);
+        }
+        return result;
+    }
+};

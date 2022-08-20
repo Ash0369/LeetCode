5fdc4ae8fdeb8e6c67187ebcca1d26f5c53c9c51
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-21 04:29:01 +0530

    Update 322.cpp

--- 322.cpp
+++ 322.cpp
@@ -93,3 +93,45 @@ public:
         return dp[n-1][amount];
     }
 };
+
+//Method-3 : Space Optimzation 
+
+
+class Solution 
+{
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int n=coins.size();
+        vector<int>dp(amount+1,1e8);
+        vector<int>curr(amount+1,1e8);
+        
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+            {
+                dp[i]=i/coins[0];
+            }
+        }
+        
+        for(int index=1;index<n;index++)
+        {
+            for(int a=0;a<=amount;a++)
+            {
+                int not_pick=0+dp[a];
+                int pick_up=INT_MAX;
+                if(a>=coins[index])
+                {
+                    pick_up=1+curr[a-coins[index]];
+                }
+                curr[a]=min(pick_up,not_pick);
+            }
+            dp=curr;
+        }
+        if(dp[amount]==1e8)
+        {
+            return -1;
+        }
+        return dp[amount];
+    }
+};

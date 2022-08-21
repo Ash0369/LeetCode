5cb98a2378dfcea2ccd04c743fda18899b217f3f
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-21 15:37:34 +0530

    Create 257.cpp

---
+++ 257.cpp
@@ -0,0 +1,34 @@
+void check(TreeNode *root,vector<string>&res,string temp)
+{
+    if(root->left==NULL && root->right==NULL)
+    {
+        //temp=temp+"->";
+        //temp=temp+(to_string(root->val));
+        res.push_back(temp);
+    }
+    if(root->left)
+    {
+        string a=temp+"->"+to_string(root->left->val);
+        check(root->left,res,a);
+    }
+    if(root->right)
+    {
+        temp=temp+"->"+to_string(root->right->val);
+        check(root->right,res,temp);
+    }
+}
+class Solution 
+{
+public:
+    vector<string> binaryTreePaths(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return {};
+        }
+        vector<string>res;
+        string temp=to_string(root->val);
+        check(root,res,temp);
+        return res;
+    }
+};

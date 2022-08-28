621021eca781f93b8fd9c70a58ac9f4824ddf5b4
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-08-28 05:57:33 +0530

    Update 771.py

--- 771.py
+++ 771.py
@@ -14,4 +14,12 @@ class Solution(object):
             if(jewels.count(x)!=0):
                 count+=1
         return count
-        
+  
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count=0
+        for x in stones:
+            if(x in jewels):
+                count+=1
+        return count

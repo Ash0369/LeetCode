ac8ea1419eb9d58ab6e4d70d59d81a1d62ec934e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-01 21:05:42 +0530

    Update 222.cpp

--- 222.cpp
+++ 222.cpp
@@ -1,4 +1,4 @@
-//Method-1
+//Method-1 :
 
 int sum(TreeNode* root)
 {
@@ -20,7 +20,7 @@ public:
     }
 };
 
-//Method-2
+//Method-2 : 
 
 int count(TreeNode* root)
 {
@@ -42,7 +42,7 @@ public:
     }
 };
 
-//Method-3 
+//Method-3 :
 
 int c=0;
 void count_nodes(TreeNode* root)
@@ -63,5 +63,51 @@ public:
         c=0;
         count_nodes(root);
         return c;
+    }
+};
+
+
+//Method-4 : Efficient
+
+pair<int,int> get_height(TreeNode *root)
+{
+    TreeNode *temp=root;
+    int right=0;
+    int left=0;
+    while(root!=NULL)
+    {
+        right++;
+        root=root->right;
+    }
+    while(temp!=NULL)
+    {
+        right++;
+        temp=temp->left;
+    }
+    
+    return make_pair(left,right);
+}
+int count_nodes(TreeNode* root)
+{
+    if(root==NULL)
+    {
+       return 0; 
+    }
+    
+    auto x=get_height(root);
+    if(x.first==x.second)
+    {
+        return pow(2,x.first)-1;
+    }
+    int a=count_nodes(root->left);
+    int b=count_nodes(root->right);
+    return a+b+1;
+}
+class Solution
+{
+public:
+    int countNodes(TreeNode* root) 
+    {
+       return count_nodes(root);
     }
 };

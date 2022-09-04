c6332547cbe6da7c85e690e85ea10c0df2a3e383
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-04 07:03:31 +0530

    Update 1143.cpp

--- 1143.cpp
+++ 1143.cpp
@@ -67,3 +67,37 @@ public:
         return dp[n1][n2];
     }
 };
+
+
+//Method-3 : Space Optimization
+
+class Solution 
+{
+public:
+    int longestCommonSubsequence(string text1,string text2) 
+    {
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<int>dp(n2+1,0);
+        vector<int>curr(n2+1,0);
+        
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                       curr[j]=1+dp[j-1];
+                }
+                else
+                {
+                    int a=dp[j];
+                    int b=curr[j-1];
+                    curr[j]=max(a,b);
+                }
+             }
+            dp=curr;
+        }
+        return dp[n2];
+    }
+};

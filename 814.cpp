5cf5c5d8b4bf9b1e6999337d7ca98650d9ce7b7e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-06 12:43:46 +0530

    Update 814.cpp

--- 814.cpp
+++ 814.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool prun(TreeNode* &root)
 {
     if(root==NULL)
@@ -37,5 +39,32 @@ public:
             return NULL;
         }
         return root;
+    }
+};
+
+
+//Method-2 : 
+
+
+TreeNode* prun(TreeNode* &root)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    root->left=prun(root->left);
+    root->right=prun(root->right);
+    if(root->left==NULL && root->right==NULL && root->val!=1)
+    {
+        return NULL;
+    }
+    return root;
+}
+class Solution 
+{
+public:
+    TreeNode* pruneTree(TreeNode* root) 
+    {
+        return prun(root);
     }
 };

408dee09669100fafc391b0fe329b0916bfbb754
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-07 14:33:32 +0530

    Update 1475.cpp

--- 1475.cpp
+++ 1475.cpp
@@ -21,3 +21,28 @@ public:
         return res;
     }
 };
+
+
+//Method-2 : Using Monotonic Stack
+
+class Solution 
+{
+public:
+    vector<int> finalPrices(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<int>res=prices;
+        stack<int>st;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()>prices[i])
+                st.pop();
+            
+            if(!st.empty())
+                res[i]=res[i]-st.top();
+            
+            st.push(prices[i]);
+        }
+        return res;
+    }
+};

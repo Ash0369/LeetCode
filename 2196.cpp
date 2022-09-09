82a7262e9ae388bb2b29767bdc2a9a565b42e688
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-09 13:53:39 +0530

    Update 2196.cpp

--- 2196.cpp
+++ 2196.cpp
@@ -49,3 +49,48 @@ public:
         return root;
     }
 };
+
+
+//Method-2 :
+
+class Solution 
+{
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) 
+    {
+        unordered_set<int>pat;
+        unordered_set<int>chd;
+        unordered_map<int,TreeNode *>mp;
+        for(auto x:descriptions)
+        {
+            TreeNode *parent;
+            if(mp.find(x[0])==mp.end())
+            {
+                TreeNode *node=new TreeNode(x[0]);
+                mp[x[0]]=node;
+            }
+            
+            TreeNode *child;
+            if(mp.find(x[1])==mp.end())
+            {
+                TreeNode *node=new TreeNode(x[1]);
+                mp[x[1]]=node;
+            }
+            parent=mp[x[0]];
+            child=mp[x[1]];
+            if(x[2]==1)
+                parent->left=child;
+            else
+                parent->right=child;
+            
+            pat.insert(x[0]);
+            chd.insert(x[1]);
+        }
+        for(auto x:pat)
+        {
+            if(chd.find(x)==chd.end())
+                return mp[x];
+        }
+        return NULL;
+    }
+};

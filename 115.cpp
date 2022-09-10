7065843cf6f7f8f70020e3384d1ec714906bf999
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-11 05:28:43 +0530

    Create 115.cpp

---
+++ 115.cpp
@@ -0,0 +1,33 @@
+int distinct(int i,int j,string &s,string &t,vector<vector<int>>&dp)
+{
+    if(j<0)
+        return 1;
+    if(i<0)
+        return 0;
+    
+    if(dp[i][j]==-1)
+    {
+        if(s[i]==t[j])
+        {
+            int a=distinct(i-1,j-1,s,t,dp);
+            int b=distinct(i-1,j,s,t,dp);
+            dp[i][j]=a+b;
+        }
+        else
+        {
+            dp[i][j]=distinct(i-1,j,s,t,dp);
+        }
+    }
+    return dp[i][j];
+}
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int m=s.length();
+        int n=t.length();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return distinct(m-1,n-1,s,t,dp);
+    }
+};

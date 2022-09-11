1fee4d6e4a20e640acb73414d7d6477c8d3a8321
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-11 06:00:46 +0530

    Update 115.cpp

--- 115.cpp
+++ 115.cpp
@@ -34,3 +34,125 @@ public:
         return distinct(m-1,n-1,s,t,dp);
     }
 };
+
+
+//Method-2 : Tabulation
+
+#define ll unsigned long long
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int m=s.length();
+        int n=t.length();
+        vector<vector<ll int>>dp(m+1,vector<ll int>(n+1,0));
+        
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int j=1;j<=n;j++)
+        {
+            dp[0][j]=0; //We can remove this also as everything is intialized with 0 , but to show proper conversion of all base cases of recursion we have kept it.
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];  
+    }
+};
+
+
+//Method-3 : Tabulation(Countering overflow)
+
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int m=s.length();
+        int n=t.length();
+        vector<vector<double>>dp(m+1,vector<double>(n+1,0));
+        
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int j=1;j<=n;j++)
+        {
+            dp[0][j]=0; //We can remove this also as everything is intialized with 0 , but to show proper conversion of all base cases of recursion we have kept it.
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[m][n];  
+    }
+};
+
+//Method-4 : Space Optimization
+
+
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int m=s.length();
+        int n=t.length();
+        vector<double>dp(n+1,0);
+        vector<double>curr(n+1,0);
+          
+        for(int j=1;j<=n;j++)
+        {
+            curr[j]=0;
+            dp[j]=0;
+        }
+        dp[0]=1;
+        curr[0]=1;
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    curr[j]=dp[j-1]+dp[j];
+                }
+                else
+                {
+                    curr[j]=dp[j];
+                    dp[j]=dp[j];
+                }
+            }
+            dp=curr;
+        }
+        return (int)dp[n];  
+    }
+};

4ecf264461b06a2376bd466154f070daf10128e2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-14 14:19:20 +0530

    Update 1641.cpp

--- 1641.cpp
+++ 1641.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int count(int n,char c, map<pair<int,char>,int>&mp)
 {
     if(n==0)
@@ -28,5 +30,42 @@ public:
     {
         map<pair<int,char>,int>mp;
         return count(n,'z',mp);
+    }
+};
+
+
+//Method-2 : 
+
+
+int count(int n,char c, vector<vector<int>>&dp)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    if(dp[n][int(c)]==-1)
+    {
+        int sum=0;
+        vector<char>vec= {'a','e','i','o','u'};
+        for(auto x:vec)
+        {
+            if(x<=c)
+            {
+                sum=sum+count(n-1,x,dp);
+            }
+            else
+                break;
+        }
+        dp[n][int(c)]=sum;
+    }
+    return dp[n][int(c)];
+}
+class Solution 
+{
+public:
+    int countVowelStrings(int n) 
+    {
+        vector<vector<int>>dp(n+1,vector<int>(123,-1));
+        return count(n,'z',dp);
     }
 };

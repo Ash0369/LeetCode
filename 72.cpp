fc7604178ba91662ab39c8851e0e9eac49fb1d47
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-15 06:35:59 +0530

    Update 72.cpp

--- 72.cpp
+++ 72.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Memoization
+
 int distance(string &word1,string &word2,int i,int j,vector<vector<int>>&vec)
 {
     //(i,j) signify no of operation to match word1 upto i index with word2 upto j index
@@ -27,5 +29,49 @@ public:
         int n=word2.size();
         vector<vector<int>>vec(m,vector<int>(n,-1));
         return distance(word1,word2,m-1,n-1,vec);
+    }
+};
+
+
+//Method-2 : Tabulation
+
+class Solution 
+{
+public:
+    int minDistance(string word1, string word2) 
+    {
+        int m=word1.size();
+        int n=word2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int j=0;j<=n;j++)
+        {
+            dp[0][j]=j; //1st base case where string 1 is exhausted
+        }
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=i; //2nd base case where string 2 is exhausted
+        }
+         
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                //Copy the recurance 
+                
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    int delete_op=1+dp[i-1][j];
+                    int insert_op=1+dp[i][j-1];
+                    int replace_op=1+dp[i-1][j-1];
+                    dp[i][j]=min(delete_op,min(insert_op,replace_op));
+                }
+                
+            }
+        }
+        return dp[m][n];
     }
 };

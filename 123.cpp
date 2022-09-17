87e6def4bd8244a1064a94ad4d9ecc40a2a13432
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-18 05:09:21 +0530

    Update 123.cpp

--- 123.cpp
+++ 123.cpp
@@ -83,3 +83,45 @@ public:
         return profit(prices,0,0,0,dp);
     }
 };
+
+
+//Method-3 : Meomization
+//Putting cap while selling for more optimization
+
+
+int res=0;
+int profit(vector<int>&prices,int hold,int index,int transaction,vector<vector<vector<int>>>&dp)
+{
+    int pr=0;
+    if(index==prices.size() || transaction==0)
+        return 0;
+    
+    if(dp[index][hold][transaction]==-1)
+    {
+        if(hold==0)
+        {
+            pr=profit(prices,1,index+1,transaction,dp)-prices[index];
+        }
+        else if(hold==1)
+        {
+            
+            pr=profit(prices,0,index+1,transaction-1,dp)+prices[index];
+            
+        }
+        int npr=0;    
+        npr=profit(prices,hold,index+1,transaction,dp);   
+        dp[index][hold][transaction]=max(pr,npr);
+    }
+    return dp[index][hold][transaction];
+}
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        res=0;
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(4,-1)));
+        return profit(prices,0,0,2,dp);
+    }
+};

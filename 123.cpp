cac642683828fc3ee447ad2c02670a659d74136e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-18 04:54:52 +0530

    Update 123.cpp

--- 123.cpp
+++ 123.cpp
@@ -1,4 +1,4 @@
-//Method-1 : 
+//Method-1 : Memoization
 
 int res=0;
 int profit(vector<int>&prices,int hold,int index,int transaction,vector<vector<vector<int>>>&dp)
@@ -35,6 +35,51 @@ public:
         res=0;
         int n=prices.size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return profit(prices,0,0,0,dp);
+    }
+};
+
+//Method-2 : Optimized Memoization
+
+int res=0;
+int profit(vector<int>&prices,int hold,int index,int transaction,vector<vector<vector<int>>>&dp)
+{
+    int pr=0;
+    if(index==prices.size())
+        return 0;
+    
+    if(dp[index][hold][transaction]==-1)
+    {
+        if(hold==0 && transaction<2)
+        {
+            pr=profit(prices,1,index+1,transaction+1,dp)-prices[index];
+        }
+        else if(hold==1)
+        {
+            if(transaction==2)
+            {
+                pr=prices[index];
+                res=max(res,pr);
+            }
+            else
+            {
+                pr=profit(prices,0,index+1,transaction,dp)+prices[index];
+            }
+        }
+        int npr=0;    
+        npr=profit(prices,hold,index+1,transaction,dp);   
+        dp[index][hold][transaction]=max(pr,npr);
+    }
+    return dp[index][hold][transaction];
+}
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        res=0;
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(4,-1)));
         return profit(prices,0,0,0,dp);
     }
 };

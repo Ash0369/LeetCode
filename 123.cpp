b945f43b2aa3a65a2b4e6f6aa6f0767fa8f334da
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-18 05:37:01 +0530

    Update 123.cpp

--- 123.cpp
+++ 123.cpp
@@ -181,3 +181,41 @@ public:
         return dp[0][0][2];//Our intial call will contain the answer    
     }
 };
+
+
+//Method-5 : Space Optimization
+
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        res=0;
+        int n=prices.size();     
+        vector<vector<int>>dp(2,vector<int>(3,0));
+        vector<vector<int>>curr(2,vector<int>(3,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=1;j>=0;j--)
+            {
+                for(int k=2;k>0;k--)
+                {
+                    int pr=0;
+                    if(j==0)
+                    {
+                        pr=dp[1][k]-prices[i];
+                    }
+                    else if(j==1)
+                    {
+                        pr=dp[0][k-1]+prices[i];     
+                    }
+                    int npr=dp[j][k];    
+                    curr[j][k]=max(pr,npr);
+                }
+            }
+            dp=curr;
+        }        
+        return dp[0][2];  
+    }
+};

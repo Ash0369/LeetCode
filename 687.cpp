74be0b3ffc761475973f90633065f338a77561ef
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-19 06:53:18 +0530

    Update 687.cpp

--- 687.cpp
+++ 687.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int ans=1;
 int check(TreeNode *&root)
 {
@@ -36,5 +38,36 @@ public:
             return 0;
         ans=1;
         return max(ans,check(root))-1;
+    }
+};
+
+
+//Method-2 : 
+
+int ans=0;
+int check(TreeNode *&root,int value)
+{
+    if(root==NULL)
+        return 0;
+    
+    int l=check(root->left,root->val);
+    int r=check(root->right,root->val);   
+    ans=max(ans,l+r);
+    
+    if(root->val==value)
+    {
+        return max(l,r)+1;
+    }
+    return 0;
+    
+    
+}
+class Solution 
+{
+public:
+    int longestUnivaluePath(TreeNode* root) 
+    {
+        ans=0;
+        return max(ans,check(root,1001));
     }
 };

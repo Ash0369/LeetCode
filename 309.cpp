89168163049d52ec1e220843c11c31b9a835202c
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-19 05:38:35 +0530

    Update 309.cpp

--- 309.cpp
+++ 309.cpp
@@ -1,3 +1,6 @@
+//Method-1 : 
+
+
 int mprofit(vector<int>&profit,int index,int transaction,vector<vector<int>>&dp)
 {
     int p=0;
@@ -32,5 +35,43 @@ public:
         int n=prices.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         return mprofit(prices,0,0,dp);
+    }
+};
+
+
+//Method-2 : 
+
+
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int p=0;
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int transaction=n-1;transaction>=0;transaction--)
+            {
+                if(transaction%3==0)
+                {
+                    p=dp[index+1][transaction+1]-prices[index];
+                }
+                else if(transaction>0 && (transaction-1)%3==0)
+                {
+                    p=dp[index+1][transaction+1]+prices[index];
+                }
+                else
+                {
+                    dp[index][transaction]=dp[index+1][transaction+1];
+                    continue;
+                }
+                int np=dp[index+1][transaction];
+                
+                dp[index][transaction]=max(p,np);
+            }
+        }
+        return dp[0][0];
     }
 };

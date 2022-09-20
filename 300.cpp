9bb59126fabc1298adebd614750308386d3bc2e2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-21 04:54:10 +0530

    Update 300.cpp

--- 300.cpp
+++ 300.cpp
@@ -27,3 +27,32 @@ public:
         return longest(nums,0,-1,dp);
     }
 };
+
+
+//Method-2 : Tabulation
+
+
+class Solution 
+{
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+2,0));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int prev=index-1;prev>=-1;prev--)
+            {
+                int pick=0;
+                if(prev==-1 || nums[index]>nums[prev])
+                    pick=1+dp[index+1][index+1];
+                
+                
+                int not_pick=dp[index+1][prev+1];
+                
+                dp[index][prev+1]=max(pick,not_pick);
+            }
+        }
+        return dp[0][0];
+    }
+};

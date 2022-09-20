2422a1bf34988db2d2ce6eed8509e080b2bee8a6
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-20 09:19:31 +0530

    Create 2096.cpp

---
+++ 2096.cpp
@@ -0,0 +1,54 @@
+TreeNode* lca(TreeNode *root,int &start,int &end)
+{
+    if(root==NULL)
+        return NULL;
+    if(root->val==start || root->val==end)
+        return root;
+    TreeNode* l=lca(root->left,start,end);
+    TreeNode* r=lca(root->right,start,end);
+    
+    if(l!=NULL && r!=NULL)
+        return root;
+   
+    if(l!=NULL)
+        return l;
+    return r;
+}
+
+bool reach(TreeNode *root,int value,string &s)
+{
+    if(root==NULL)
+        return false;
+    if(root->val==value)
+        return true;
+    bool a=reach(root->left,value,s);
+    bool b=reach(root->right,value,s);
+    
+    if(a)
+        s.push_back('L');
+    if(b)
+        s.push_back('R');
+    
+    return a|b;
+}
+class Solution 
+{
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) 
+    {
+        TreeNode *ancestor=lca(root,startValue,destValue);
+        string start,end;
+        reach(ancestor,startValue,start);
+        reach(ancestor,destValue,end);
+        
+        string res="";
+        for(auto x:start)
+            res.push_back('U');
+         reverse(end.begin(),end.end());
+         for(auto x:end)
+            res.push_back(x);
+        
+        return res;
+
+    }
+};

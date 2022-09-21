6b821978fe6d60acc29c153c7ad1fcdb49fc2eb8
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-21 22:29:24 +0530

    Update 1283.cpp

--- 1283.cpp
+++ 1283.cpp
@@ -46,3 +46,48 @@ public:
         return res;
     }
 };
+
+
+//Method-2 : 
+
+bool smallest(vector<int>&nums,int &mid,int &threshold)
+{
+    int sum=0;
+    for(int i=0;i<nums.size();i++)
+    {
+        if(nums[i]%mid==0)
+            sum=sum+(nums[i]/mid);
+        else
+            sum=sum+(nums[i]/mid)+1;
+        
+        if(sum>threshold)
+            return false;
+    }
+    return true;
+}
+class Solution 
+{
+public:
+    int smallestDivisor(vector<int>&nums, int threshold) 
+    {
+        int max_num=*max_element(nums.begin(),nums.end());
+        int low=1;
+        int high=max_num;
+        int res=INT_MAX;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            
+            if(smallest(nums,mid,threshold))
+            {
+                res=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+};

cb2f6db0dd0cda7f179e600962851a31eea3e5a8
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-22 07:00:19 +0530

    Update 437.cpp

--- 437.cpp
+++ 437.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -20,6 +22,37 @@ public:
         dfs(root,targetSum);
         pathSum(root->left,targetSum);
         pathSum(root->right,targetSum);        
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    long long int ans=0;
+    unordered_map<long long int ,int>mp;
+    void dfs(TreeNode *root,int target,long long int sum)
+    {
+        if(root==NULL)
+            return;
+        
+        sum=sum+root->val;
+        if(sum==target)
+            ans++;
+        if(mp.find(sum-target)!=mp.end())
+            ans=ans+mp[sum-target];
+        mp[sum]++;
+        dfs(root->left,target,sum);
+        dfs(root->right,target,sum);
+        
+        mp[sum]--;
+    }
+    int pathSum(TreeNode* root, int targetSum) 
+    {
+        dfs(root,targetSum,0);        
         return ans;
     }
 };

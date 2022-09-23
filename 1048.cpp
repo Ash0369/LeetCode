da703ae1bc794717b7b630de72ee6b3b50d10826
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-24 05:12:17 +0530

    Create 1048.cpp

---
+++ 1048.cpp
@@ -0,0 +1,53 @@
+bool check(string &s1 ,string &s2)
+{
+    if(s1.size()!=s2.size()+1)
+        return false;
+    
+    int f=0;
+    int s=0;
+    int count=0;
+    while(f<s1.size())
+    {
+        if(s1[f]!=s2[s])
+        {
+            f++;
+        }
+        else
+        {
+            f++;
+            s++;
+        }
+    }
+    
+    return (f==s1.size() && s==s2.size());
+}
+
+bool comp(string &s1 , string &s2)
+{
+    return s1.size()<s2.size();
+}
+class Solution 
+{
+public:
+    int longestStrChain(vector<string>&nums) 
+    {
+        int n=nums.size();
+        sort(nums.begin(),nums.end(),comp);//Sort according to length
+        vector<int>dp(n+1,1); 
+        int ans=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int prev=0;prev<i;prev++)
+            {
+               // if(nums[prev]<nums[i])
+                if(check(nums[i],nums[prev]))
+                {
+                    dp[i]=max(dp[i],1+dp[prev]);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        
+        return ans;    
+    }
+};

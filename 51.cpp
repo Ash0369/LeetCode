643aad9aca642e8f91fd0d02c7ec881c105aab11
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-24 07:03:19 +0530

    Update 51.cpp

--- 51.cpp
+++ 51.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool safe(vector<string>&board,int row,int column,int n)
 {
     //Other then this 3 direction Queen cannot be placed in remaing because that are later moves.
@@ -61,6 +63,56 @@ public:
             board[i]=s;
         }
         solve(0,board,ans,n);
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+
+void solve(int column,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftrow, vector<int>&ld,vector<int>&ud,int n)
+{
+    if(column==n)
+    {
+        ans.push_back(board);
+        return;
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(leftrow[i]==0 && ld[column+i]==0 && ud[n-1+column-i]==0)
+        {
+            
+            board[i][column]='Q';
+            leftrow[i]=1;
+            ld[column+i]=1;
+            ud[n-1+column-i]=1;
+            solve(column+1,board,ans,leftrow,ld,ud,n);
+            board[i][column]='.';
+            leftrow[i]=0;
+            ld[column+i]=0;
+            ud[n-1+column-i]=0;
+        }
+    }
+}
+class Solution 
+{
+public:
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        vector<vector<string>>ans;
+        vector<string>board(n);
+        string s(n,'.');
+        
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        vector<int>leftrow(n,0);
+        vector<int>ud((2*n)-1,0);
+        vector<int>ld((2*n)-1,0);
+        solve(0,board,ans,leftrow,ld,ud,n);
         return ans;
     }
 };

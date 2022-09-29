bbdbcd6aacaadddb8623fca66d94e64866453db3
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-29 06:54:39 +0530

    Update 337.cpp

--- 337.cpp
+++ 337.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 #define ma make_pair 
 int cash(TreeNode *root,int status, map<pair<TreeNode*,int>,int>&mp)
 {
@@ -22,6 +24,39 @@ int cash(TreeNode *root,int status, map<pair<TreeNode*,int>,int>&mp)
     }
     return mp[ma(root,status)];
     
+}
+
+class Solution 
+{
+public:
+    int rob(TreeNode* root) 
+    {
+        map<pair<TreeNode*,int>,int>mp;
+        return cash(root,0,mp);
+    }
+};
+
+
+//Method-2 : 
+
+#define ma make_pair 
+int cash(TreeNode *root,int status, map<pair<TreeNode*,int>,int>&mp)
+{
+    if(root==NULL)
+        return 0;
+
+    if(mp.find(ma(root,status))==mp.end())
+    {
+        int l=0,l1=0,lw=0;
+        if(status==0)
+        {
+            l=root->val+cash(root->left,1,mp)+cash(root->right,1,mp);
+        }
+        lw=cash(root->left,0,mp)+cash(root->right,0,mp);
+        
+        mp[ma(root,status)]=max(l,lw);
+    }
+    return mp[ma(root,status)];
 }
 
 class Solution 

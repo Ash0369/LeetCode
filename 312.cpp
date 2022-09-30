67b893199306274a69d74ae8550e925f9e882a53
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-09-30 06:14:46 +0530

    Update 312.cpp

--- 312.cpp
+++ 312.cpp
@@ -30,3 +30,35 @@ public:
         return distribute(1,n,nums,dp);
     }
 };
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int maxCoins(vector<int>& nums) 
+    {
+        int n=nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(),1);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n;j++)//j should be greter
+            {
+                if(j<i)
+                    continue;
+                int ans=0;
+                int coin=0;
+                for(int index=i;index<=j;index++)
+                {
+                   coin=(nums[i-1]*nums[index]*nums[j+1])+dp[i][index-1] + dp[index+1][j];
+                   ans=max(ans,coin);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[1][n];
+    }
+};

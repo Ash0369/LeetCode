194ada0ebb1c68f570d1780b5e3d90dc78b352aa
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-01 15:11:51 +0530

    Create 1277.cpp

---
+++ 1277.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+public:
+    int countSquares(vector<vector<int>>& matrix) 
+    {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        int sum=0;
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[0][i]==1)
+            {
+                sum++;
+                dp[0][i]=1;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(matrix[i][0]==1)
+            {
+                sum++;
+                dp[i][0]=1;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]==1)
+                {
+                    dp[i][j]=min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]))+1;
+                    sum=sum+dp[i][j];
+                }
+            }
+        }
+        
+        return sum;
+    }
+};

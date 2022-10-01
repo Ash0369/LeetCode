cc725e19ce999bb5bf793c1464187855ea3853a0
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-01 11:56:03 +0530

    Update 84.cpp

--- 84.cpp
+++ 84.cpp
@@ -1,84 +1,41 @@
-class Solution {
+class Solution 
+{
 public:
     int largestRectangleArea(vector<int>& heights) 
     {
         int n=heights.size();
-        stack<int>index;
-        vector<int>left_limit(n,0);
-        vector<int>right_limit(n,0);
-
-        //Left to Right
+        stack<int>st;
+        vector<int>left;
+        vector<int>right;
         for(int i=0;i<n;i++)
         {
-            int a=heights[i];
-            int left=0;
-            if(index.empty())
-            {
-                index.push(i);
-                left=0;
-            }
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty())
+                left.push_back(0);
             else
-            {
-                while(!index.empty() && heights[index.top()]>=a)
-                {
-                    index.pop();
-                }
-                if(index.empty())
-                {
-                    left=0;
-                }
-                else
-                {
-                    left=index.top()+1;
-                }
-                index.push(i);
-            }
-            left_limit[i]=left;
+                left.push_back(st.top()+1);
+            
+            st.push(i);
         }
-
-        while(!index.empty())
+        stack<int>st2;
+        for(int i=n-1;i>=0;i--)
         {
-            index.pop();
-        }
-
-        
-        //Right to left
-        for(int i=(n-1);i>=0;i--)
-        {
-            int a=heights[i];
-            int right=n-1;
-            if(index.empty())
-            {
-                index.push(i);
-                right=n-1;
-            }
+            while(!st2.empty() && heights[st2.top()]>=heights[i])
+                st2.pop();
+            if(st2.empty())
+                right.push_back(n-1);
             else
-            {
-                while(!index.empty() && heights[index.top()]>=a)
-                {
-                    index.pop();
-                }
-                if(index.empty())
-                {
-                    right=n-1;
-                }
-                else
-                {
-                    right=index.top()-1;
-                }
-                index.push(i);
-            }
-            right_limit[i]=right;
+                right.push_back(st2.top()-1);
+            
+            st2.push(i);
         }
-
         int max_area=0;
+        reverse(right.begin(),right.end());
         for(int i=0;i<n;i++)
         {
-            int temp=(right_limit[i]-left_limit[i]+1)*heights[i];
-            if(temp>max_area)
-            {
-                max_area=temp;
-            }
+            int area=(right[i]-left[i]+1)*heights[i];
+            max_area=max(max_area,area);
         }
         return max_area;
     }

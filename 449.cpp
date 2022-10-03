b097d8449dbec7537e1e8963a2f0d8096f914b3c
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-03 06:54:42 +0530

    Update 449.cpp

--- 449.cpp
+++ 449.cpp
@@ -1,3 +1,53 @@
+//Method-1 : Optimal
+
+class Codec 
+{
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) 
+    {
+        if(!root)
+            return "";
+        
+        return to_string(root->val)+'-'+serialize(root->left)+serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    
+    TreeNode *make_tree(TreeNode *root,int value)
+    {
+        if(root==NULL)
+        {
+            TreeNode *temp=new TreeNode(value);
+            return temp;
+        }
+        if(root->val<value)
+        {
+            root->right=make_tree(root->right,value);
+        }
+        else
+        {
+            root->left=make_tree(root->left,value);
+        }
+        return root;
+    }
+    TreeNode* deserialize(string data) 
+    {
+        if(data.size()==0 || data[0]=='*')
+            return NULL;
+        stringstream s(data);
+        string d;
+        TreeNode *root=NULL;
+        while(getline(s,d,'-'))
+            root=make_tree(root,stoi(d));
+        
+        return root;
+    }
+};
+
+//Method-2 : (High Space)
+
 class Codec 
 {
 public:

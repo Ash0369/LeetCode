18b5ec37346fed12f3ff3b08d5599d97f9c91e80
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-03 06:38:59 +0530

    Create 449.cpp

---
+++ 449.cpp
@@ -0,0 +1,74 @@
+class Codec 
+{
+public:
+    string serialize(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return "";
+        }
+        queue<TreeNode *>q;
+        q.push(root);
+        string res="";
+        while(!q.empty())
+        {
+            
+            TreeNode *r=q.front();
+            q.pop();
+            if(r==NULL)
+            {
+                res.append("*,");
+                continue;
+            }
+            res.append(to_string(r->val)+',');     
+            q.push(r->left);
+            q.push(r->right);
+            
+        }
+        return res;
+    }
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) 
+    {
+        if(data.size()==0)
+        {
+            return NULL;
+        }
+        stringstream s(data);
+        string str;
+        getline(s,str,',');
+        queue<TreeNode *>q;
+        TreeNode *root =new TreeNode(stoi(str));
+        q.push(root);
+        while(!q.empty())
+        {
+          
+              TreeNode *t=q.front();
+              q.pop();
+              getline(s,str,',');
+              if(str=="*")
+              {
+                  t->left=NULL;
+              }
+              else
+              {
+                  TreeNode *temp=new TreeNode(stoi(str));
+                  t->left=temp;
+                  q.push(temp);
+              }
+              getline(s,str,',');
+              if(str=="*")
+              {
+                  t->right=NULL;
+              }
+              else
+              {
+                  TreeNode *temp=new TreeNode(stoi(str));
+                  t->right=temp;
+                  q.push(temp);
+              }
+        }
+        
+        return root;
+    }
+};

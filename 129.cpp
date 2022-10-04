e1fb7d67b9e485351c490fd2919d17816d21a15d
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-05 04:29:53 +0530

    Update 129.cpp

--- 129.cpp
+++ 129.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int sum(TreeNode *root,int value)
 {
     if(root==NULL)
@@ -8,6 +10,30 @@ int sum(TreeNode *root,int value)
     }
     int a=root->val;
     value=(10*value)+a;
+    return sum(root->left,value)+sum(root->right,value);
+}
+class Solution 
+{
+public:
+    int sumNumbers(TreeNode* root) 
+    {
+        return sum(root,0);
+    }
+};
+
+
+//Method-2 : 
+
+
+int sum(TreeNode *root,int value)
+{
+    if(root==NULL)
+        return 0;
+    value=(10*value)+root->val;
+    if(root->left==root->right)
+    {
+        return value;
+    }
     return sum(root->left,value)+sum(root->right,value);
 }
 class Solution 

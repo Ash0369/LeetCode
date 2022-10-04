e900fa14ca981ab83be23656bc9cccad2888df43
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-04 05:37:25 +0530

    Update 109.cpp

--- 109.cpp
+++ 109.cpp
@@ -1,3 +1,6 @@
+//Method-1:
+
+
 TreeNode *make(ListNode* head)
 {
     if(head==NULL)
@@ -29,5 +32,38 @@ public:
     TreeNode* sortedListToBST(ListNode* head) 
     {
         return make(head);
+    }
+};
+
+
+//Method-2 : 
+
+int count(ListNode *head)
+{
+    if(head==NULL)
+        return 0;
+    return 1+count(head->next);
+}
+TreeNode *make(ListNode* &head,int n)
+{
+    if(n==0)
+        return NULL;
+    
+    TreeNode *lft=make(head,n/2);
+    TreeNode *root=new TreeNode(head->val);
+    head=head->next;
+    root->left=lft;
+    
+    root->right=make(head,n-(n/2)-1);
+    
+    return root;
+}
+class Solution 
+{
+public:
+    TreeNode* sortedListToBST(ListNode* head) 
+    {
+        int n=count(head);
+        return make(head,n);
     }
 };

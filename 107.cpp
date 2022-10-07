00fc3c3e4e19b9e475ce4bd67451f9f399bb563c
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-07 05:33:42 +0530

    Update 107.cpp

--- 107.cpp
+++ 107.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Using reverse
+
 class Solution 
 {
 public:
@@ -26,6 +28,47 @@ public:
             temp.clear();
         }
         reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//Method-2 : Using Stack
+
+class Solution 
+{
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) 
+    {
+        if(root==NULL)
+            return {};
+        vector<vector<int>>ans;
+        vector<int>temp;
+        queue<TreeNode*>st;
+        st.push(root);
+        stack<vector<int>>s;
+        while(!st.empty())
+        {
+            int k=st.size();
+            for(int i=0;i<k;i++)
+            {
+                TreeNode *t=st.front();
+                temp.push_back(t->val);
+                if(t->left)
+                    st.push(t->left);
+                if(t->right)
+                    st.push(t->right);
+                st.pop();
+            }
+            s.push(temp);
+            temp.clear();
+        }
+        while(!s.empty())
+        {
+            ans.push_back(s.top());
+            s.pop();
+        }
+        
         return ans;
     }
 };

b68a482c17b4bcdba558419153bffd32879a9201
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-10 17:22:30 +0530

    Update 729.cpp

--- 729.cpp
+++ 729.cpp
@@ -23,3 +23,29 @@ public:
         return true;
     }
 };
+
+
+//Method-2 : Binary Search
+
+class MyCalendar 
+{
+public:
+    map<int,int>mp;
+    set<pair<int,int>>st;
+    MyCalendar() 
+    {
+        
+    }
+    
+    bool book(int start, int end) 
+    {
+        auto p=mp.upper_bound(start);
+        if(p==mp.end() || end<=p->second)
+        {
+            mp[end]=start;
+            return true;
+        }
+        return false;
+        
+    }
+};

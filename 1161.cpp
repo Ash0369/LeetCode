ab495622e3c8e861b7894121b007c0574865a4f2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-12 16:04:56 +0530

    Update 1161.cpp

--- 1161.cpp
+++ 1161.cpp
@@ -36,3 +36,36 @@ public:
         return level;
     }
 };
+
+
+//Method-2 : DFS
+
+class Solution 
+{
+public:
+    int sum[100];
+    int mx_level=0;
+    void dfs(TreeNode *root,int level)
+    {
+      
+        mx_level=max(mx_level,level);
+        sum[level]=sum[level]+root->val;
+        if(root->left) dfs(root->left,level+1);
+        if(root->right) dfs(root->right,level+1);
+    }
+    int maxLevelSum(TreeNode* root) 
+    {
+        int level=0;
+        int val=INT_MIN;
+        dfs(root,0);
+        for(int i=0;i<=mx_level;i++)
+        {
+            if(sum[i]>val)
+            {
+                val=sum[i];
+                level=i;
+            }
+        }
+        return level+1;
+    }
+};

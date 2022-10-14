239476bbfe80b370cfd7932c0306c71bcd110f17
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-14 10:44:26 +0530

    Update 547.cpp

--- 547.cpp
+++ 547.cpp
@@ -37,3 +37,39 @@ public:
         return count;
     }
 };
+
+
+//Method-2 : DFS
+
+class Solution 
+{
+public:
+    int cnt=0;
+    void dfs(vector<vector<int>>&m,int i,vector<bool>&vis)
+    {
+        vis[i]=true;
+        for(int j=0;j<m.size();j++)
+        {
+            if(m[i][j]==1 && vis[j]==false)
+            {
+                dfs(m,j,vis);
+            }
+            
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int n=isConnected.size();
+        vector<bool>vis(n+1,false);
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==false)
+            {
+                cnt++;
+                dfs(isConnected,i,vis);
+            }
+        }
+        
+        return cnt;
+    }
+};

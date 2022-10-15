a502f5e3d6f6f2caa387173ab58e4eb14f8083f7
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-16 05:09:41 +0530

    Update 733.cpp

--- 733.cpp
+++ 733.cpp
@@ -1,3 +1,6 @@
+//Method-1 : BFS
+
+
 #define mp(a,b) make_pair(a,b)
 class Solution 
 {
@@ -43,6 +46,49 @@ public:
                 }
             }
         }
+        return image;
+    }
+};
+
+
+//Method-2 : DFS
+
+
+vector<int>delx={-1,0,1,0};
+vector<int>dely={0,1,0,-1};
+void dfs(vector<vector<int>>& image,int x,int y,int color,vector<vector<int>>&visited,int intial_color)
+{
+    int m=image.size();
+    int n=image[0].size();
+    
+    image[x][y]=color;
+    
+    for(int i=0;i<4;i++)
+    {
+        if(x+delx[i]>=0 && x+delx[i]<m && y+dely[i]>=0 && y+dely[i]<n)
+        {
+            if(visited[x+delx[i]][y+dely[i]]==0 && image[x+delx[i]][y+dely[i]]==intial_color)
+            {
+                visited[x+delx[i]][y+dely[i]]=1;
+                dfs(image,x+delx[i],y+dely[i],color,visited,intial_color);
+            }
+        }
+        
+    }
+}
+class Solution 
+{
+public:
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        int m=image.size();
+        int n=image[0].size();
+        
+        vector<vector<int>>visited(60,vector<int>(60,0));
+        int intial_color=image[sr][sc];
+        visited[sr][sc]=1;
+        dfs(image,sr,sc,color,visited,intial_color);
         return image;
     }
 };

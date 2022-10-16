ec9de4591d01cfbd66816362173670fa10ee3016
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-16 09:55:24 +0530

    Update 1335.cpp

--- 1335.cpp
+++ 1335.cpp
@@ -34,3 +34,46 @@ public:
         return ans;
     }
 };
+
+
+//Method-2 : Memoization
+
+
+int diff(vector<int>& job,int d,int index,int sum,vector<vector<int>>&dp)
+{
+    if(d==1)
+    {
+        int ele=0;
+        for(int i=index;i<job.size();i++)
+        {
+            ele=max(ele,job[i]);
+        }
+        return dp[index][1]=ele;
+    }
+    if(dp[index][d]!=-1)
+        return dp[index][d];
+    int grt=0;
+    int ans=INT_MAX;
+    for(int i=index;job.size()-i>=d;i++)
+    {
+        grt=max(grt,job[i]);  
+        ans=min(ans, grt+diff(job,d-1,i+1,sum+grt,dp));
+    }
+    
+    return dp[index][d]=ans;
+}
+class Solution 
+{
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) 
+    {
+        int sum=0;
+        int n=jobDifficulty.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(11,-1));
+        int ans=diff(jobDifficulty,d,0,0,dp);
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
+    }
+};

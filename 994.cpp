8bac17c8921023aa87ee758f30deca0f9c4e9441
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-16 05:57:05 +0530

    Update 994.cpp

--- 994.cpp
+++ 994.cpp
@@ -1,3 +1,5 @@
+//Method-1 :
+
 #define mp(a,b) make_pair(a,b)
 class Solution 
 {
@@ -54,6 +56,68 @@ public:
                     return -1;
             }
         }
+        return max(0,time-1);
+    }
+};
+
+
+//Method-2 : Eliminating Checking Loop
+
+#define mp(a,b) make_pair(a,b)
+class Solution 
+{
+public:
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        int counter=0;//To count
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push(mp(i,j));
+                    counter++;
+                }
+                if(grid[i][j]==0)
+                    counter++;//Count empty cell also
+            }
+        }
+        vector<int>dx={-1,0,1,0};
+        vector<int>dy={0,1,0,-1};
+        int time=0;
+        while(!q.empty())
+        {
+            int k=q.size();
+            time++;
+            
+            for(int i=0;i<k;i++)
+            {
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                
+                for(int j=0;j<4;j++)
+                {
+                    int nx=x+dx[j];
+                    int ny=y+dy[j];
+                    
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1)
+                    {
+                        q.push(mp(nx,ny));
+                        grid[nx][ny]=2;
+                        counter++;
+                    }
+                }
+            }
+        }
+        
+        if(counter!=(n*m))
+            return -1;
+        
         return max(0,time-1);
     }
 };

c336916c8971cccff6db6f89ff7dcc34c1983fa4
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-21 14:30:27 +0530

    Update 938.cpp

--- 938.cpp
+++ 938.cpp
@@ -1,3 +1,5 @@
+//Method-1:
+
 int sum=0;
 void check(TreeNode *root,int low,int high)
 {
@@ -8,6 +10,39 @@ void check(TreeNode *root,int low,int high)
         sum+=n;
     check(root->left,low,high);
     check(root->right,low,high);
+}
+class Solution 
+{
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) 
+    {
+        sum=0;
+        check(root,low,high);
+        return sum;
+    }
+};
+
+
+//Method-2:
+
+
+int sum=0;
+void check(TreeNode *root,int &low,int &high)
+{
+    if(!root)
+        return;
+    int n=root->val;
+    if(n>=low && n<=high)
+    {
+        sum+=n;
+        check(root->left,low,high);
+        check(root->right,low,high);
+    }
+        
+    else if(n>high)
+        check(root->left,low,high);
+    else if(n<low)
+        check(root->right,low,high);
 }
 class Solution 
 {

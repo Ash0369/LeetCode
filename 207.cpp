fe63e3b2ff4d32629176b4b61d85189cd4eb0353
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-22 05:44:32 +0530

    Update 207.cpp

--- 207.cpp
+++ 207.cpp
@@ -1,3 +1,6 @@
+//Method-1 : DFS
+
+
 bool dfs(int node,vector<int>adj[],vector<int>&visited,vector<int>&pvisited)
 {
     visited[node]=true;
@@ -40,5 +43,64 @@ public:
             }
         }
         return true;
+    }
+};
+
+
+//Method-2 : BFS
+
+class Solution 
+{
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        int n=numCourses;
+        vector<int>visited(n,false);
+        vector<int>pvisited(n,false);
+        vector<int>adj[n];
+        
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        
+        vector<int>indegree(n,0);
+	    queue<int>q;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(auto x:adj[i])
+	        {
+	            indegree[x]++;
+	        }
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        if(indegree[i]==0)
+	        {
+	            q.push(i);
+	            
+	        }
+	    }
+	    int cnt=0;
+	    while(!q.empty())
+	    {
+	        int node=q.front();
+	        q.pop();
+	        cnt++;
+	        
+	        for(auto x:adj[node])
+	        {
+	            indegree[x]--;
+	            if(indegree[x]==0)
+	            {
+	                q.push(x);
+	            }
+	        }
+	    }
+	    if(cnt==n)
+	    {
+	        return true;
+	    }
+	    return false;
     }
 };

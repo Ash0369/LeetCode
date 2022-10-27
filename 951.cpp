9f660128ec615e31736fd14c2df5d4f4fa836a3b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-27 16:47:09 +0530

    Update 951.cpp

--- 951.cpp
+++ 951.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool check(TreeNode *&root1,TreeNode *&root2)
 {
     if(root1==NULL && root2==NULL)
@@ -10,6 +12,34 @@ bool check(TreeNode *&root1,TreeNode *&root2)
             return true;
         swap(root1->left,root1->right);
         if(check(root1->left,root2->left) && check(root1->right,root2->right))
+            return true;
+    }
+    return false;
+}
+class Solution 
+{
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) 
+    {
+        return check(root1,root2);
+    }
+};
+
+
+//Method-2 : Without using Swap
+
+
+bool check(TreeNode *&root1,TreeNode *&root2)
+{
+    if(root1==NULL && root2==NULL)
+        return true;
+    if(root1==NULL || root2==NULL)
+        return false;
+    if(root1->val==root2->val)
+    {
+        if(check(root1->left,root2->left) && check(root1->right,root2->right))
+            return true;
+        if(check(root1->left,root2->right) && check(root1->right,root2->left))
             return true;
     }
     return false;

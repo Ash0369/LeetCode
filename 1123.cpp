08f541123abbd0986172fcb9aa882ee241192c33
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-10-30 12:34:28 +0530

    Update 1123.cpp

--- 1123.cpp
+++ 1123.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Using pair
+
 pair<int,TreeNode*>deep(TreeNode *root,int depth,TreeNode *parent)
 {
     if(root==NULL)
@@ -17,5 +19,37 @@ public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) 
     {
         return deep(root,0,root).second;
+    }
+};
+
+
+//Method-2 : Using less space
+
+
+int mx_depth=0;
+TreeNode *lca;
+int deep(TreeNode *root,int depth)
+{
+    int l=depth;
+    int r=depth;
+    if(root->left)
+        l=deep(root->left,depth+1);
+    if(root->right)
+        r=deep(root->right,depth+1);
+    
+    mx_depth=max(mx_depth,depth);
+    
+    if(l==mx_depth && r==mx_depth)
+        lca=root;
+    return max(l,r);
+}
+class Solution 
+{
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) 
+    {
+        mx_depth=0;
+        deep(root,0);
+        return lca;
     }
 };

3ac918ff8eff8a4cf162b122b102a585505b8197
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-01 12:44:45 +0530

    Update 127.cpp

--- 127.cpp
+++ 127.cpp
@@ -44,3 +44,44 @@ public:
         return ans+1;
     }
 };
+
+
+//Method-2 -->BFS It will give TLE after 34 Test case but will pass in GFG
+
+class Solution 
+{
+public:
+    int wordLadderLength(string beginWord, string endWord, vector<string>& wordList) 
+    {
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        set<string>st;
+        for(auto x:wordList)
+            st.insert(x);
+        while(!q.empty())
+        {
+            auto node=q.front();
+            q.pop();
+            string s=node.first;
+            int pos=node.second;
+            if(s==endWord)
+                return pos;
+            st.erase(s);
+            
+            for(int j=0;j<s.length();j++)
+            {
+                for(int i=97;i<=126;i++)
+                {
+                    char c=char(i);
+                    string temp=s;
+                    temp[j]=c;
+                    if(st.find(temp)!=st.end())
+                    {
+                        q.push({temp,pos+1});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};

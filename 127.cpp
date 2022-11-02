86ccab7dbf3041f156bd48b03f60a23a8e8a1f19
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-02 07:00:42 +0530

    Update 127.cpp

--- 127.cpp
+++ 127.cpp
@@ -1,63 +1,16 @@
-//Method-1 : DFS --> It will give TLE after test case 25
-
-
-int op(string &start,string &dest,int cnt,set<string>&st,map<string,int>&visited)
-{
-    if(start==dest && cnt!=0)
-        return cnt;
-    int ans=INT_MAX;
-    visited[start]=cnt;
-    for(int i=0;i<start.size();i++)
-    {
-        for(int j=97;j<=122;j++)
-        {
-            if(start[i]!=char(j))
-            {
-                string temp=start;
-                temp[i]=char(j);
-                if(st.find(temp)!=st.end() && (visited.find(temp)==visited.end() || visited[temp]>cnt))
-                {
-                    int a=op(temp,dest,cnt+1,st,visited);
-                    ans=min(ans,a);
-                }
-            }
-        }
-    }
-    return ans;
-}
-class Solution 
-{
-public:
-    int ladderLength(string beginWord, string endWord, vector<string>&wordList) 
-    {
-        set<string>st;
-        map<string,int>visited;
-        for(auto x:wordList)
-        {
-            st.insert(x);
-        }
-        if(st.find(endWord)==st.end())
-            return 0;
-        int ans=op(beginWord,endWord,0,st,visited);
-        if(ans==INT_MAX)
-            return 0;
-        return ans+1;
-    }
-};
-
-
-//Method-2 -->BFS It will give TLE after 34 Test case but will pass in GFG
+//Method-1
 
 class Solution 
 {
 public:
-    int  ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
     {
         queue<pair<string,int>>q;
         q.push({beginWord,1});
         set<string>st;
         for(auto x:wordList)
             st.insert(x);
+        st.erase(beginWord);
         while(!q.empty())
         {
             auto node=q.front();
@@ -66,7 +19,7 @@ public:
             int pos=node.second;
             if(s==endWord)
                 return pos;
-            st.erase(s);
+            
             
             for(int j=0;j<s.length();j++)
             {
@@ -78,6 +31,7 @@ public:
                     if(st.find(temp)!=st.end())
                     {
                         q.push({temp,pos+1});
+                        st.erase(temp);
                     }
                 }
             }
@@ -87,7 +41,7 @@ public:
 };
 
 
-//Method-3 : Passed
+//Method-2:
 
 class Solution 
 {

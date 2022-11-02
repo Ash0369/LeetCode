4d171d01761e86bdc9c4129775e4a9cf06dcc0d6
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-02 11:58:10 +0530

    Update 1248.cpp

--- 1248.cpp
+++ 1248.cpp
@@ -1,3 +1,5 @@
+//Method-1: 
+
 class Solution 
 {
 public:
@@ -33,6 +35,46 @@ public:
             }
             left=intial;
         
+        }
+        return cnt;
+    }
+};
+
+
+//Method-2 : 
+
+
+class Solution 
+{
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) 
+    {
+        int left=0;
+        int n=nums.size();
+        int cnt=0;
+        int odd=0;
+        int temp=0;
+        for(int right=0;right<n;right++)
+        {
+            if(nums[right]%2!=0)
+            {
+                odd++;
+                if(odd>=k)
+                {
+                    temp=1;
+                    while(nums[left]%2==0)
+                    {
+                        temp++;
+                        left++;
+                    }
+                    left++;
+                    cnt+=temp;
+                }
+            }
+            else if(odd>=k)
+            {
+                cnt+=temp;
+            }
         }
         return cnt;
     }

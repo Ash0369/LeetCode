722240c3f08787109098c53c2cbfdce82b64eb30
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-02 06:57:56 +0530

    Update 127.cpp

--- 127.cpp
+++ 127.cpp
@@ -85,3 +85,51 @@ public:
         return 0;
     }
 };
+
+
+//Method-3 : Passed
+
+class Solution 
+{
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+    {
+        queue<string>q;
+        map<string,int>mp;
+        mp[beginWord]=1;
+        q.push(beginWord);
+        set<string>st;
+        for(auto x:wordList)
+            st.insert(x);
+        st.erase(beginWord);
+        int pos=2;
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int iter=0;iter<n;iter++)
+            {
+                auto node=q.front();
+                q.pop();
+                string s=node;
+                
+                for(int j=0;j<s.length();j++)
+                {
+                    for(int i=97;i<=126;i++)
+                    {
+                        char c=char(i);
+                        string temp=s;
+                        temp[j]=c;
+                        if(st.find(temp)!=st.end())
+                        {
+                            q.push(temp);
+                            st.erase(temp);
+                            mp[temp]=pos;
+                        }
+                    }
+                }
+            }
+            pos++;
+        }
+        return mp[endWord];
+    }
+};

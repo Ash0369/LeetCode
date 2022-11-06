11f4e4a60931d536d1d0fc3a736c4a6057b16451
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-06 10:02:15 +0530

    Update 547.cpp

--- 547.cpp
+++ 547.cpp
@@ -73,3 +73,115 @@ public:
         return cnt;
     }
 };
+
+
+//Method-3 : DSU
+
+class dsu
+{
+    public:
+    vector<int>rank,parent,size;
+    
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+
+};
+
+class Solution 
+{
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        int n=isConnected.size();
+        dsu ds(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    ds.union_by_rank(i,j);
+                }
+            }
+        }
+        
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.parent[i]==i)
+                cnt++;
+        }
+        return cnt;
+    }
+};

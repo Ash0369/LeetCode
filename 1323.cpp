a69b1f7220a7d7e2ba2014584c634768534e9ac3
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-07 14:03:28 +0530

    Update 1323.cpp

--- 1323.cpp
+++ 1323.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -24,6 +26,38 @@ public:
             }
             ans=ans*10 + a;
         }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int maximum69Number (int num) 
+    {
+        int dup=num;
+        bool op=true;
+        int last6=-1;
+        int cnt=0;
+        while(num>0)
+        {
+            int a=num%10;
+            if(a==6)
+                last6=cnt;
+            num=num/10;
+            cnt++;
+        }
+        num=dup;
+        int ans=0;
+        if(last6==-1)
+            return num;
+        ans=num/pow(10,last6+1);
+        ans=ans*10 + 9;
+        
+        ans=ans*(pow(10,last6)) + (num%int(pow(10,last6)));
         return ans;
     }
 };

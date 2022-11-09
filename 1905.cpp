a3c70e17e607854173da0ab52615fc57bfc22a59
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-09 15:35:07 +0530

    Update 1905.cpp

--- 1905.cpp
+++ 1905.cpp
@@ -1,3 +1,5 @@
+//Method-1 : BFS
+
 class Solution 
 {
 public:
@@ -54,6 +56,71 @@ public:
                             }
                         }
                     }
+                    if(check)
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+};
+
+
+//Method-2 : DFS
+
+
+bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j,vector<vector<bool>>&dp)
+{
+    int m=grid2.size();
+    int n=grid2[0].size();
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    int check=true;
+    if(grid1[i][j]==0)
+        check=false;
+    for(int k=0;k<4;k++)
+    {
+        int nx=i+dx[k];
+        int ny=j+dy[k];
+        
+        if(nx>=0 && ny>=0 && nx<m && ny<n && grid2[nx][ny]==1 && dp[nx][ny]==false)
+        {
+            dp[nx][ny]=true;
+            
+            bool temp=dfs(grid1,grid2,nx,ny,dp);
+            if(!temp || grid1[nx][ny]==0)
+            {
+                check=false;
+            }
+        }
+    }
+    return check;
+    
+    
+}
+class Solution 
+{
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) 
+    {
+        int m=grid2.size();
+        int n=grid2[0].size();
+        vector<vector<bool>>dp(m,vector<bool>(n,false));
+        int counter=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid2[i][j]==1 && dp[i][j]==false)
+                {
+                    dp[i][j]=true;
+                    bool check=true;
+                    check=dfs(grid1,grid2,i,j,dp);
+                    if(grid1[i][j]==0)
+                        check=false;
+                   
                     if(check)
                     {
                         counter++;

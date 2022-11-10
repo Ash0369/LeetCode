13458fa85cbdcd918480c16b0fa7676d3495fb70
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-10 17:30:33 +0530

    Update 1267.cpp

--- 1267.cpp
+++ 1267.cpp
@@ -1,3 +1,5 @@
+//Method-1 : DSU
+
 class dsu
 {
     public:
@@ -116,6 +118,62 @@ public:
         {
             if(ds.parent[i]==i && ds.size[i]>1)
                 ans=ans+ds.size[i];
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : DFS
+
+int dfs(int row,int col,int m,int n,vector<vector<int>>& grid,vector<vector<bool>>&dp)
+{
+    int cnt=0;
+    //keep row constant and visit
+    for(int j=0;j<n;j++)
+    {
+        if(dp[row][j]==false && grid[row][j]==1)
+        {
+            dp[row][j]=true;
+            cnt++;
+            cnt+=dfs(row,j,m,n,grid,dp);
+        }
+    }
+    
+    //keep col constant and visit
+    for(int i=0;i<m;i++)
+    {
+        if(dp[i][col]==false && grid[i][col]==1)
+        {
+            dp[i][col]=true;
+            cnt++;
+            cnt+=dfs(i,col,m,n,grid,dp);
+        }
+    }
+    return cnt;
+}
+class Solution 
+{
+public:
+    int countServers(vector<vector<int>>& grid) 
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
+        vector<vector<bool>>dp(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(dp[i][j]==false && grid[i][j]==1)
+                {
+                    dp[i][j]=true;
+                    int temp=dfs(i,j,m,n,grid,dp);
+                    if(temp>0)
+                        ans+=temp+1;
+                    
+                }
+            }
         }
         return ans;
     }

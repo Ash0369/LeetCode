b6ed7c97ecef4cfa082ae41ab9c2f62b26590e96
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-16 11:58:15 +0530

    Create 2076.cpp

---
+++ 2076.cpp
@@ -0,0 +1,121 @@
+//Method-1 : Brute Force (Keep checking all request in restricted array almost O(n*n) solution)
+
+
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+
+};
+class Solution 
+{
+public:
+    vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) 
+    {
+        dsu ds(n+1);
+        vector<bool>res;
+        
+        for(int i=0;i<requests.size();i++)
+        {
+            int a=requests[i][0];
+            int b=requests[i][1];
+            bool ans=true;
+            for(int j=0;j<restrictions.size();j++)
+            {
+                int n1=restrictions[j][0];
+                int n2=restrictions[j][1];
+                
+                if(ds.find_parent(n1)==ds.find_parent(a) && ds.find_parent(n2)==ds.find_parent(b))
+                {
+                    ans=false;
+                    break;
+                }
+                if(ds.find_parent(n1)==ds.find_parent(b) && ds.find_parent(n2)==ds.find_parent(a))
+                {
+                        ans=false;
+                        break;
+                }  
+            }
+            if(ans)
+            {
+                 res.push_back(true);
+                 ds.union_by_rank(a,b);
+            }
+            else
+                res.push_back(false);
+        }
+        return res;
+    }
+};

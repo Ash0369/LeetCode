b5ff106c58e23f3b3917e3862cf4ee8b361bf741
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-18 10:54:07 +0530

    Create 1971.cpp

---
+++ 1971.cpp
@@ -0,0 +1,90 @@
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+
+};
+class Solution 
+{
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        dsu ds(n+1);
+        for(auto x:edges)
+            ds.union_by_rank(x[0],x[1]);
+        return ds.find_parent(source)==ds.find_parent(destination);
+            
+    }
+};

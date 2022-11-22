cace81a90b54f01aaed250c530d4f65d6a084d87
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-22 09:44:39 +0530

    Update 334.cpp

--- 334.cpp
+++ 334.cpp
@@ -1,26 +1,24 @@
-//Method-1 : Recursive - High Time Complexity so Large Test Case will not pass
-
-
-bool search(vector<int>&nums,int index,int prev,int count)
-{
-    if(count==3)
-        return true;
-    if(index==nums.size())
-        return false;
-    
-    bool b=search(nums,index+1,prev,count);
-    bool a=false;
-    if(nums[index]>prev)
-        a=search(nums,index+1,nums[index],count+1);
-    
-    return a|b;
-   
-}
 class Solution 
 {
 public:
     bool increasingTriplet(vector<int>& nums) 
     {
-        return search(nums,0,INT_MIN,0);
+        if(nums.size()<3)
+            return false;
+        int left=INT_MAX;
+        int mid=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>mid)
+                return true;
+            
+            if(nums[i]>left && nums[i]<mid)
+            {
+                mid=nums[i];
+            }
+            else if(nums[i]<left)
+                left=nums[i];
+        }
+        return false;
     }
 };

5772cec80553993d878f3ea11a7c53fd901877b7
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-11-30 16:44:58 +0530

    Update 1207.cpp

--- 1207.cpp
+++ 1207.cpp
@@ -3,8 +3,8 @@ class Solution
 public:
     bool uniqueOccurrences(vector<int>& arr) 
     {
-        map<int,int>mp;
-        set<int>st;
+        unordered_map<int,int>mp;
+        unordered_set<int>st;
         for(auto x:arr)
             mp[x]++;
         for(auto x:mp)

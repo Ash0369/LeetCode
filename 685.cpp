9ecf3ea92d31cd2357af0ee67b7d5a7ce412d93e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-10 15:37:21 +0530

    Update 685.cpp

--- 685.cpp
+++ 685.cpp
@@ -156,3 +156,168 @@ public:
         return edges[ans];
     }
 };
+
+
+//Method-2 : Optimized 
+//https://www.youtube.com/watch?v=d0tqBMRZ6UQ&t=893s
+
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+    
+    void union_by_order(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(ultimate_parent_u<ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(ultimate_parent_u>ultimate_parent_v)
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(ultimate_parent_u==ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+    }
+
+};
+class Solution 
+{
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) 
+    {
+        int n=edges.size();
+        int ans;
+        vector<int>indegree(n+1,-1);
+        int blacklist1=-1;
+        int blacklist2=-1;
+        dsu ds(n+1);
+        
+        for(int j=0;j<n;j++)
+        {
+            int u=edges[j][0];
+            int v=edges[j][1];
+            
+            if(indegree[v]==-1)
+            {
+                indegree[v]=j;
+            }
+            else
+            {
+                blacklist1=j;//Which is now give extra indegree
+                blacklist2=indegree[v];//Which already given indegree;
+                break;
+            }
+            
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i==blacklist1)
+                continue;
+            
+            int u=edges[i][0];
+            int v=edges[i][1];
+            if(ds.find_parent(u)==ds.find_parent(v))
+            {
+                if(blacklist1==-1)
+                {
+                    //We don't have any blacklist edge but then also cycle.
+                    //So cureent ith edge we have to remove
+                    
+                    return edges[i];
+                }
+                //We already skipped blacklist1 edge then also cycle is present.
+                //So we have to remove backlist2
+                
+                return edges[blacklist2];
+            }
+            ds.union_by_rank(u,v);
+        }
+        
+        //We have reached here means we have correctly blacklisted1
+        return edges[blacklist1];
+    }
+};

a5b46c57e14dcf68490bcbd51d430b2bafca4fd8
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-11 13:47:09 +0530

    Create 1998.cpp

---
+++ 1998.cpp
@@ -0,0 +1,134 @@
+class dsu
+{
+    vector<int>rank,parent,size;
+    public:
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+    
+    void union_by_order(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(ultimate_parent_u<ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(ultimate_parent_u>ultimate_parent_v)
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(ultimate_parent_u==ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+    }
+
+};
+class Solution 
+{
+public:
+    bool gcdSort(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int>keep=nums;
+        sort(nums.begin(),nums.end());
+        int mx=nums[n-1];
+        dsu ds(mx+1);
+        for(int i=0;i<n;i++)
+        {
+            int val=nums[i];
+            for(int j=2;j<=sqrt(val);j++)
+            {
+                if(val%j==0)
+                {
+                     ds.union_by_rank(val,j);
+                     ds.union_by_rank(val,val/j);
+                }
+               
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(ds.find_parent(keep[i])!=ds.find_parent(nums[i]))
+                return false;
+        }
+        return true;
+        
+    }
+};

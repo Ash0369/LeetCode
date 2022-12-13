9bed30df777e67c07ed437edce5fd4be3d03cbcd
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-13 22:03:23 +0530

    Create 2316.cpp

---
+++ 2316.cpp
@@ -0,0 +1,139 @@
+//Method-1 : DSU
+
+class dsu
+{
+    public:
+    vector<int>rank,parent,size;
+    dsu(int n)
+    {
+        rank.resize(n+1,0);//Work for both 0 or 1 based
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    int find_parent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=find_parent(parent[node]);//Path compression
+    }
+
+    void union_by_rank(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(rank[ultimate_parent_u]<rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(rank[ultimate_parent_u]>rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(rank[ultimate_parent_u]==rank[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            rank[ultimate_parent_u]++;
+        }
+    }
+
+    void union_by_size(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(size[ultimate_parent_u]<size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+            size[ultimate_parent_v]+=size[ultimate_parent_u];
+        }
+        else if(size[ultimate_parent_u]>size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+        else if(size[ultimate_parent_u]==size[ultimate_parent_v])
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+            size[ultimate_parent_u]+=size[ultimate_parent_v];
+        }
+    }
+    
+    void union_by_order(int u,int v)
+    {
+        int ultimate_parent_u=find_parent(u);
+        int ultimate_parent_v=find_parent(v);
+
+        if(ultimate_parent_u==ultimate_parent_v)
+        {
+            //They are belonging to same component
+            return;
+        }
+        else if(ultimate_parent_u<ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+        else if(ultimate_parent_u>ultimate_parent_v)
+        {
+            parent[ultimate_parent_u]=ultimate_parent_v;
+        }
+        else if(ultimate_parent_u==ultimate_parent_v)
+        {
+            parent[ultimate_parent_v]=ultimate_parent_u;
+        }
+    }
+
+};
+class Solution 
+{
+public:
+    long long countPairs(int n, vector<vector<int>>& edges) 
+    {
+        dsu ds(n);
+        for(auto x:edges)
+        {
+            ds.union_by_size(x[0],x[1]);
+        }
+        vector<int>vec;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.find_parent(i)==i)
+            {
+                vec.push_back(ds.size[i]);
+            }
+        }
+        long long int ans=0;
+        long long int full=0;
+        for(int i=0;i<vec.size();i++)
+        {
+            full+=vec[i];
+        }
+        for(int i=0;i<vec.size();i++)
+        {
+            ans+=(full-vec[i])*vec[i];
+            //Like if 1 4 2
+            //Full is 7
+            //So for 2 we have 5 choices (full-vec[i])
+            //And that 5 choices with 2 so *vec[i];
+        }
+        return ans/2;
+    }
+};

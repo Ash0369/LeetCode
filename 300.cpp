f5481f06e8c5ff7dbfb210bc0eb3747ad8eee8fe
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-20 15:00:49 +0530

    Update 300.cpp

--- 300.cpp
+++ 300.cpp
@@ -198,3 +198,31 @@ public:
         
     }
 };
+
+
+//Method-7 : 
+
+class Solution 
+{
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int>dp(n+1,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=(i-1);j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans=max(ans,dp[i]);
+       return ans+1;  
+    }
+};

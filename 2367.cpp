37339339a36c62db1659884790b1520806db2347
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-22 12:40:50 +0530

    Create 2367.cpp

---
+++ 2367.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) 
+    {
+        int n=nums.size();
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            int a=nums[i];
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[j]==(a+diff))
+                {
+                    int b=nums[j];
+                    for(int k=j+1;k<n;k++)
+                    {
+                        if(nums[k]==(b+diff))
+                            cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};

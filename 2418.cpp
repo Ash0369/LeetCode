961fea72b0efb816d9ea32213549f535764e496b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-22 21:42:29 +0530

    Update 2418.cpp

--- 2418.cpp
+++ 2418.cpp
@@ -1,3 +1,5 @@
+//Method-1 :
+
 class Solution 
 {
 public:
@@ -14,6 +16,30 @@ public:
         for(int i=0;i<heights.size();i++)
         {
             ans.push_back(names[mp[heights[i]]]);
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
+    {
+        priority_queue<pair<int,string>>pq;
+        for(int i=0;i<names.size();i++)
+        {
+            pq.push({heights[i],names[i]});
+        }
+        
+        vector<string>ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }

576c2fa2232fcc8b5ad2f4140df34565ca7e6f89
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-23 16:22:02 +0530

    Create 1615.cpp

---
+++ 1615.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) 
+    {
+        vector<set<int>>graph(n);
+        for(auto x:roads)
+        {
+            graph[x[0]].insert(x[1]);
+            graph[x[1]].insert(x[0]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                int a=graph[i].size();
+                int b=graph[j].size();
+                if(graph[i].find(j)!=graph[i].end())
+                {
+                    ans=max(ans,a+b-1);
+                }
+                else
+                {
+                    ans=max(ans,a+b);
+                }
+            }
+        }
+        return ans;
+    }
+};

abf2a200d41674319579c29f6ec20496e2584ff7
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-26 07:19:04 +0530

    Update 55.cpp

--- 55.cpp
+++ 55.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool jump(vector<int>&nums,int index,vector<int>&dp)
 {
     if(index>=(nums.size()-1))
@@ -20,5 +22,32 @@ public:
         int n=nums.size();
         vector<int>dp(n+1,-1);
         return jump(nums,0,dp);
+    }
+};
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<bool>dp(n+1,false);
+        //dp[i]-->If it possible to reach ith step or not
+        dp[0]=true;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j]==true && nums[j]>=(i-j))
+                {
+                    dp[i]=true;
+                    break;
+                }
+            }
+            
+        }
+        return dp[n-1];
     }
 };

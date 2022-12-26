e5672cc7b6fe6db0fb43b34d5164ddd235362e32
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-26 22:13:07 +0530

    Create 2368.cpp

---
+++ 2368.cpp
@@ -0,0 +1,31 @@
+int dfs(int node,vector<vector<int>>&graph,vector<bool>&dp,set<int>&r)
+{
+    int cnt=1;
+    dp[node]=true;
+    for(auto x:graph[node])
+    {
+        if(dp[x]==false && r.find(x)==r.end())
+        {
+            cnt+=dfs(x,graph,dp,r);
+        }
+    }
+    return cnt;
+}
+class Solution 
+{
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) 
+    {
+        vector<vector<int>>graph(n);
+        set<int>r(restricted.begin(),restricted.end());
+        vector<bool>dp(n+1,false);
+        for(auto x:edges)
+        {
+            graph[x[0]].push_back(x[1]);
+            graph[x[1]].push_back(x[0]);
+        }
+        if(r.find(0)!=r.end())
+            return 0;
+        return dfs(0,graph,dp,r);
+    }
+};

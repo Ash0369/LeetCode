c66cf3239489214c83d7c4ae5884d8ed947d57df
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-27 07:19:02 +0530

    Update 2279.cpp

--- 2279.cpp
+++ 2279.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -19,6 +21,34 @@ public:
             ans++;
             additionalRocks-=demand;
             pq.pop();
+        }
+        return ans;
+    }
+};
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) 
+    {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        int n=capacity.size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            capacity[i]=capacity[i]-rocks[i];
+        }
+        sort(capacity.begin(),capacity.end());
+        for(int i=0;i<n;i++)
+        {
+            int demand=capacity[i];
+            if(demand>additionalRocks)
+                break;
+            ans++;
+            additionalRocks-=demand;
         }
         return ans;
     }

bcfce139a86f754be2669bd1f0121a63a2a63eef
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2022-12-28 22:56:54 +0530

    Create 2124.cpp

---
+++ 2124.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    bool checkString(string s) 
+    {
+        bool b=false;
+        for(auto x:s)
+        {
+            if(x=='a' && b==true)
+            {
+                return false;
+            }
+            if(x=='b')
+                b=true;
+        }
+        return true;
+    }
+};

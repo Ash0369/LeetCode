955d9904d54bb8e067f7fe374c3814a0ee88b6dd
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-03 21:08:04 +0530

    Update 944.cpp

--- 944.cpp
+++ 944.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -18,6 +20,35 @@ public:
         {
             if(!is_sorted(vec[i].begin(),vec[i].end()))
                 cnt++;
+        }
+        return cnt;
+    }
+};
+
+
+//Method-2 : 
+
+
+class Solution 
+{
+public:
+    int minDeletionSize(vector<string>& strs) 
+    {
+        int n=strs[0].size();
+        int m=strs.size();
+        
+        int cnt=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(strs[j][i]<strs[j-1][i])
+                {
+                    cnt++;
+                    break;
+                }
+            }
         }
         return cnt;
     }

305c97094a9f48e51334e831b61f2c086be13e64
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-09 10:59:22 +0530

    Update 1162.cpp

--- 1162.cpp
+++ 1162.cpp
@@ -1,3 +1,7 @@
+//Method-1 : 
+
+//Space- O(n*n)+O(n)
+
 class Solution 
 {
 public:
@@ -35,6 +39,56 @@ public:
                 if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0 && dp[nx][ny]==0)
                 {
                     dp[nx][ny]=d+1;
+                    ans=d+1;
+                    q.push({d+1,{nx,ny}});
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+//Space -O(n)
+
+class Solution 
+{
+public:
+    int maxDistance(vector<vector<int>>& grid) 
+    {
+        int n=grid.size();
+        queue<pair<int,pair<int,int>>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    q.push({0,{i,j}});
+                }
+            }
+        }
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        int ans=-1;
+        while(!q.empty())
+        {
+            auto node=q.front();
+            q.pop();
+            int x=node.second.first;
+            int y=node.second.second;
+            int d=node.first;
+            
+            for(int i=0;i<4;i++)
+            {
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0)
+                {
+                    grid[nx][ny]=1;
                     ans=d+1;
                     q.push({d+1,{nx,ny}});
                 }

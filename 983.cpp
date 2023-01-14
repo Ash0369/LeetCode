9d3e952fa0430dff6b94745ed23964783395df0c
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-14 08:11:06 +0530

    Update 983.cpp

--- 983.cpp
+++ 983.cpp
@@ -1,3 +1,5 @@
+//Method-1: 
+
 int cost(vector<int>& days, vector<int>& costs,int index,int have,vector<vector<int>>&dp)
 {
     if(index==days.size())
@@ -22,5 +24,34 @@ public:
         int n=days.size();
         vector<vector<int>>dp(n,vector<int>(400,-1));
         return cost(days,costs,0,0,dp);
+    }
+};
+
+//Method-2 : 
+
+
+class Solution 
+{
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {
+        int n=days.size();
+        vector<int>dp(400,0);
+        set<int>st(days.begin(),days.end());
+        for(int i=1;i<366;i++)
+        {
+            if(st.find(i)==st.end())
+            {
+                dp[i]=dp[i-1];
+            }
+            else
+            {
+                int a=dp[i-1]+costs[0];
+                int b=dp[max(0,i-7)]+costs[1];
+                int c=dp[max(0,i-30)]+costs[2];
+                dp[i]=min(a,min(b,c));
+            }
+        }
+        return dp[365];
     }
 };

6c550dea104f8a85aaa791a79ed96e9a2ab016ea
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-16 07:57:52 +0530

    Update 57.cpp

--- 57.cpp
+++ 57.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 bool cmp(vector<int>&vec1,vector<int>&vec2)
 {
     if(vec1[0]<vec2[0])
@@ -44,6 +46,38 @@ public:
                 ans.push_back(intervals[i]);
             }
         }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
+        int n=intervals.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(newInterval[0]>intervals[i][1])
+            {
+                ans.push_back(intervals[i]);
+            }
+            else if(newInterval[1]<intervals[i][0])
+            {
+                ans.push_back(newInterval);
+                newInterval=intervals[i];
+            }
+            else if(intervals[i][1]>=newInterval[0] || intervals[i][0]<=newInterval[1])
+            {
+                newInterval[0]=min(intervals[i][0],newInterval[0]);
+                newInterval[1]=max(newInterval[1],intervals[i][1]);
+            }
+        }
+        ans.push_back(newInterval); 
         return ans;
     }
 };

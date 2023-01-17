6386607527255e274b4e875df141a9529681be01
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-17 22:11:46 +0530

    Create 1561.cpp

---
+++ 1561.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    int maxCoins(vector<int>& piles)
+    {
+        int n=piles.size();
+        sort(piles.begin(),piles.end(),greater<int>());
+        int ans=0;
+        for(int i=1;i<piles.size();i=i+2)
+        {
+            ans+=piles[i];
+            piles.pop_back();
+        }
+        return ans;
+    }
+};

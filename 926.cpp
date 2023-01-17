ccf36b8329379c0bbfe73c76f4a198133c614c8b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-17 09:39:51 +0530

    Update 926.cpp

--- 926.cpp
+++ 926.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -33,5 +35,59 @@ public:
             ans=min(ans,op);
         }
         return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int minFlipsMonoIncr(string s) 
+    {
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(2,0));
+        dp[0][0]=0;
+        dp[0][1]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                if(j==0)
+                {
+                    //Not fliped
+                    if(s[i]=='0' && s[i-1]=='1')
+                    {
+                        dp[i][j]=dp[i-1][1];
+                    }
+                    else if(s[i]=='0' && s[i-1]=='0')
+                    {
+                        dp[i][j]=dp[i-1][0];
+                    }
+                    else
+                    {
+                        dp[i][j]=min(dp[i-1][0],dp[i-1][1]);
+                    }
+                }
+                else
+                {
+                    //Flipped
+                    if(s[i]=='1' && s[i-1]=='1')
+                    {
+                        dp[i][j]=1+dp[i-1][1];
+                    }
+                    else if(s[i]=='1' && s[i-1]=='0')
+                    {
+                        dp[i][j]=1+dp[i-1][0];
+                    }
+                    else
+                    {
+                        dp[i][j]=1+min(dp[i-1][0],dp[i-1][1]);
+                    }
+                }
+            }
+        }
+        return min(dp[n-1][0],dp[n-1][1]);
     }
 };

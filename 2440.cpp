8f85aa7056ec1b2ae605e0d301962cd9d2013419
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-01-21 11:31:39 +0530

    Update 2440.cpp

--- 2440.cpp
+++ 2440.cpp
@@ -1,3 +1,5 @@
+//Method-1 :
+
 int dfs(int val,int node,vector<vector<int>>&graph,int &del,vector<bool>&dp,vector<int>& nums)
 {
     dp[node]=true;
@@ -76,6 +78,53 @@ public:
                     ans=max(ans,del);
                 }
                 
+            }
+        }      
+        if(ans==0) 
+            return 0;
+        return ans-1;
+    }
+};
+
+
+//Method-2 : 
+
+int dfs(int val,int node,int parent,vector<vector<int>>&graph,vector<int>& nums)
+{
+    int v=nums[node];
+    for(auto x:graph[node])
+    {
+        if(x==parent)
+            continue;
+        v+=dfs(val,x,node,graph,nums);
+    }
+    if(v==val)
+        return 0;
+    return v;
+}
+class Solution 
+{
+public:
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) 
+    {
+        int n=nums.size();
+        vector<vector<int>>graph(n);
+        for(auto x:edges)
+        {
+            graph[x[0]].push_back(x[1]);
+            graph[x[1]].push_back(x[0]);
+        }
+        int ans=0;
+        int s=0;
+        for(auto x:nums)
+            s=s+x;
+        for(int i=1;i<=s;i++)
+        {
+            if(s%i!=0)
+                continue;
+            if(dfs(i,0,-1,graph,nums)==0)
+            {
+                ans=max(ans,s/i);
             }
         }      
         if(ans==0) 

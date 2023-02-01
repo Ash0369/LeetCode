1a2c6d1bd1e6f656f4e51e5cf9f2d6dd416aa8f2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-01 07:23:40 +0530

    Update 2008.cpp

--- 2008.cpp
+++ 2008.cpp
@@ -35,3 +35,46 @@ public:
         return taxi(rides,0,-1,dp);
     }
 };
+
+
+//Method-2 : 
+
+#define ll long long
+int find(int curr,vector<int>&start)
+{
+    int ind=lower_bound(start.begin(),start.end(),curr)-start.begin();
+    return ind;
+}
+ll int taxi(vector<vector<int>>& rides,int curr,vector<ll int>&dp, vector<int>&start)
+{
+    int n=rides.size();
+    if(curr==n)
+        return 0;
+    if(dp[curr]!=-1)
+    {
+        return dp[curr];
+    }
+    ll int not_pick=taxi(rides,curr+1,dp,start);
+    ll int pick=0;
+    int dest=find(rides[curr][1],start);
+    pick=rides[curr][1]-rides[curr][0]+rides[curr][2]+ taxi(rides,dest,dp,start);
+
+    return dp[curr]=max(pick,not_pick);
+    
+}
+
+class Solution 
+{
+public:
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) 
+    {
+        sort(rides.begin(),rides.end());
+        vector<int>start;
+        for(auto x:rides)
+        {
+            start.emplace_back(x[0]);
+        }
+        vector<ll int>dp(rides.size()+1,-1);
+        return taxi(rides,0,dp,start);
+    }
+};

ff748795f474301f8b3fc378c55b4315b86b0e48
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-03 09:24:31 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -38,6 +40,51 @@ public:
             {
                 ans.push_back(s[y]);
             }
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : Better speed
+
+class Solution 
+{
+public:
+    string convert(string s, int numRows) 
+    {
+        string ans="";
+        int n=s.size();
+        vector<string>dp(numRows);
+        int iter=0;
+        for(int j=0;j<s.size();j++)
+        {
+            if(iter%2==0)
+            {
+                for(int i=0;i<numRows;i++)
+                {
+                    if(j>=s.size())
+                        break;
+                    dp[i].push_back(s[j]);
+                    j++;
+                }
+            }
+            else
+            {
+                for(int i=numRows-2;i>0;i--)
+                {
+                    if(j>=s.size())
+                        break;
+                    dp[i].push_back(s[j]);
+                    j++;
+                }
+            }  
+            j--;
+            iter++;
+        }
+        for(auto x:dp)
+        {
+            ans+=x;
         }
         return ans;
     }

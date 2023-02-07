4a52cc7e7414419dc96a1de80b68b99f348b221e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-07 11:35:36 +0530

    Update 740.cpp

--- 740.cpp
+++ 740.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int earn(int index,map<int,int>&score,vector<int>&nums,vector<int>&dp)
 {
     int n=nums.size();
@@ -41,5 +43,46 @@ public:
             nums.emplace_back(x.first);
         }
         return earn(0,mp,nums,dp);
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int deleteAndEarn(vector<int>& nums) 
+    {
+        map<int,int>mp;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n+1);
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        nums.clear();
+        for(auto x:mp)
+        {
+            nums.emplace_back(x.first);
+        }
+        dp[nums.size()]=0;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            //Not pick 
+            dp[i]=dp[i+1];
+            
+            //pick
+            int new_index=i+1;
+            while(new_index<nums.size() && nums[new_index]==(nums[i]+1))
+            {
+                new_index++;
+            }
+            int val=(nums[i]*mp[nums[i]])+dp[new_index];
+            dp[i]=max(dp[i],val);
+        }
+        
+        return dp[0];
     }
 };

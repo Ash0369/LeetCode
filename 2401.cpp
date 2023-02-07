d582a1c986a9e468b06415e19adb67536c8be1e7
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-07 09:21:17 +0530

    Update 2401.cpp

--- 2401.cpp
+++ 2401.cpp
@@ -1,3 +1,6 @@
+//Method-1 : 
+
+
 bool check(vector<int>&dp)
 {
     for(int i=0;i<=30;i++)
@@ -46,6 +49,40 @@ public:
                 }
                 left++;
             }
+            ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+
+class Solution 
+{
+public:
+    int longestNiceSubarray(vector<int>& nums) 
+    {
+        //If And of all pairs should be zero then at a bit position only 1 number have set 
+        //All oher unset
+        //So keep taking OR to sum it.
+        //And if new element what we are getting is also set at same position
+        //Then unset the bits of number from left
+        vector<int>dp(31,0);
+        int left=0;
+        int n=nums.size();
+        int ans=0;
+        int sum=0;
+        for(int right=0;right<n;right++)
+        {
+            int x=nums[right];
+            while((sum&x)!=0)
+            {
+                sum=sum^nums[left];
+                left++;
+            }
+            sum=sum|x;
             ans=max(ans,right-left+1);
         }
         return ans;

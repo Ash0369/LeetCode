f7889529423856ebb3939fb103fabdde1b99935b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-10 11:43:39 +0530

    Update 1526.cpp

--- 1526.cpp
+++ 1526.cpp
@@ -1,3 +1,5 @@
+//Method-1 : Monotonic Stack
+
 class Solution 
 {
 public:
@@ -21,6 +23,28 @@ public:
             {
                 op+=target[i]-st.top();
                 st.push(target[i]);
+            }
+        }
+        return op;
+    }
+};
+
+
+//Method-2: Dynamic Programming
+
+
+class Solution 
+{
+public:
+    int minNumberOperations(vector<int>& target) 
+    {
+        int n=target.size();
+        int op=target[0];
+        for(int i=1;i<n;i++)
+        {
+            if(target[i]>target[i-1])
+            {
+                op+=target[i]-target[i-1];
             }
         }
         return op;

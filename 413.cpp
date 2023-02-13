cc9d005ad418b632c8ab0ed31d33c404a9009e71
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-13 11:09:42 +0530

    Update 413.cpp

--- 413.cpp
+++ 413.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -18,6 +20,37 @@ public:
                 {
                     break;
                 }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int ans=0;
+        if(n<3)
+        {
+            return 0;
+        }
+        int cnt=0;
+        for(int i=1;i<n-1;i++)
+        {
+            if(nums[i+1]-nums[i]==nums[i]-nums[i-1])
+            {
+                cnt++;
+                ans+=cnt;
+            }
+            else
+            {
+                cnt=0;
             }
         }
         return ans;

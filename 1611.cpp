1f442e42f96a0412adfe92c2c16ae4a3e3f0d3af
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-14 15:55:03 +0530

    Update 1611.cpp

--- 1611.cpp
+++ 1611.cpp
@@ -1,4 +1,7 @@
 //https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/discuss/877741/C%2B%2B-solution-with-explanation
+
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -15,5 +18,22 @@ public:
         }
         int maxop=pow(2,bit+1-1)-1;
         return maxop-minimumOneBitOperations(n-(1<<(bit-1)));
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int minimumOneBitOperations(int n) 
+    {
+        int res=0; 
+        for(int temp=n;temp!= 0;temp=temp/2)
+        { 
+            res^=temp;
+        }
+        return res;
     }
 };

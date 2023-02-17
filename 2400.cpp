b8afda877a481132a7b0acbe9afcb14e9f876afc
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-02-17 20:51:34 +0530

    Update 2400.cpp

--- 2400.cpp
+++ 2400.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 int mod=1e9+7;
 int way(int curr,int end,int k,vector<vector<int>>&dp)
 {
@@ -27,5 +29,38 @@ public:
         endPos+=1000;
         vector<vector<int>>dp(3005,vector<int>(1005,-1));
         return way(startPos,endPos,k,dp);
+    }
+};
+
+
+//Method-2 :
+
+int mod=1e9+7;
+int way(int diff,int k,vector<vector<int>>&dp)
+{
+    if(diff==0 && k==0)
+    {
+        return 1;
+    }
+    if(k==0)
+    {
+        return 0;
+    }
+    if(dp[diff][k]!=-1)
+    {
+        return dp[diff][k];
+    }
+    int a=way(diff+1,k-1,dp);
+    int b=way(abs(diff-1),k-1,dp);
+    
+    return dp[diff][k]=(a+b)%mod;
+}
+class Solution 
+{
+public:
+    int numberOfWays(int startPos, int endPos, int k) 
+    {
+        vector<vector<int>>dp(2005,vector<int>(1005,-1));
+        return way(abs(startPos-endPos),k,dp);
     }
 };

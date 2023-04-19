3bace4ab75587299d9c832a72c898f04c5d5130e
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-04-19 16:29:37 +0530

    Update 215.cpp

--- 215.cpp
+++ 215.cpp
@@ -1,4 +1,7 @@
-class Solution {
+//Method-1 : 
+
+class Solution 
+{
 public:
     int findKthLargest(vector<int>& nums, int k) 
     {
@@ -14,5 +17,44 @@ public:
         }
         return pq.top();
 
+    }
+};
+
+
+//Method-2 : 
+
+int indx;
+int quick(vector<int>&arr,int l,int r)
+{
+   
+    int grt=l;
+    int pivot=arr[r];
+    for(int i=l;i<r;i++)
+    {
+        if(arr[i]>pivot)
+        {
+            continue;
+        }
+        swap(arr[i],arr[grt]);
+        grt++;
+    }
+    swap(arr[r],arr[grt]);
+    if(indx==grt)
+    {
+        return arr[indx];
+    }
+    else if(indx>grt)
+    {
+        return quick(arr,grt+1,r);
+    }
+    return quick(arr,l,grt-1);
+}
+class Solution 
+{
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        indx=nums.size()-k;
+        return quick(nums,0,nums.size()-1);
     }
 };

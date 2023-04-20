6ce60f8483254be38f2acc29abc808ab7c2f0ec4
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-04-20 06:24:27 +0530

    Update 1639.cpp

--- 1639.cpp
+++ 1639.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 #define ll long long
 const int mod=1e9+7;
 int dp[1001][1001];
@@ -44,5 +46,41 @@ public:
         }
         memset(dp,-1,sizeof(dp));
         return ways(0,0,vec,target);
+    }
+};
+
+
+//Method-2 : 
+
+#define ll long long
+const int mod=1e9+7;
+class Solution 
+{
+public:
+    int numWays(vector<string>& words, string &target) 
+    {
+        int n=words[0].size();
+        vector<vector<int>>vec(n,vector<int>(26,0));
+        for(int i=0;i<words.size();i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                vec[j][words[i][j]-'a']++;
+            }
+        }
+        ll int dp[1001][1001];//dp[i][j] taking first i char and forming first j char
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0]=1;//Not perfoming anything
+            for(int j=1;j<=target.size();j++)
+            {
+                ll int cnt=vec[i-1][target[j-1]-'a'];
+                dp[i][j]=(dp[i-1][j])+ (cnt*(dp[i-1][j-1]))%mod;
+                dp[i][j]=dp[i][j]%mod;
+            }
+        }
+        return dp[n][target.size()];
     }
 };

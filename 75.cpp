b29a011baa779beb3e4d0ce90dcbec06d41fda6f
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-04-20 09:36:23 +0530

    Update 75.cpp

--- 75.cpp
+++ 75.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -27,6 +29,39 @@ public:
             {
                 swap(nums[i],nums[ptr]);
                 ptr++;
+            }
+        }
+    }
+};
+
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
     }

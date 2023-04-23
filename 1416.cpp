e0e0d607098917a27ff58b589b88865e7abf5aea
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-04-23 06:55:50 +0530

    Update 1416.cpp

--- 1416.cpp
+++ 1416.cpp
@@ -1,3 +1,5 @@
+//Memoization
+
 const int mod=1e9+7;
 int dfs(string &s,int k,int index,vector<int>&dp)
 {
@@ -39,5 +41,44 @@ public:
     {
         vector<int>dp(s.size()+2,-1);
         return dfs(s,k,0,dp);
+    }
+};
+
+
+//Recursive
+
+const int mod=1e9+7;
+class Solution 
+{
+public:
+    int numberOfArrays(string s, int k) 
+    {
+        int n=s.size();
+        vector<int>dp(n+2,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            string temp="";
+            if(s[i]=='0')
+            {
+                dp[i]=0;
+                continue;
+            }
+            for(int j=0;j<=10;j++)
+            {
+                if(i+j>=n)
+                {
+                    break;
+                }
+                temp.push_back(s[i+j]);
+                long long int num=stoll(temp);
+                if(num>k)
+                {
+                    break;
+                }
+                dp[i]=(dp[i]+dp[i+j+1])%mod;
+            }
+        }
+        return dp[0];
     }
 };

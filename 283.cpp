ecf3460e12fa4bae31d734a317fc6c653b529c85
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-05-17 19:15:19 +0530

    Create 283.cpp

---
+++ 283.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int j=0;
+        while(j<n && nums[j]!=0)
+        {
+            j++;
+        }
+        if(j==n)
+        {
+            return;
+        }
+        for(int i=j+1;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};

1455788c88369c056694e5f420e147fd7f970b6b
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-05-30 18:31:44 +0530

    Update 48.cpp

--- 48.cpp
+++ 48.cpp
@@ -1,3 +1,5 @@
+//Method-1 :
+
 class Solution 
 {
 public:
@@ -25,5 +27,35 @@ public:
             }
         }
         
+    }
+};
+
+//Method-2 :
+
+class Solution 
+{
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=j)
+                {
+                    continue;
+                }
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                swap(matrix[i][j],matrix[i][n-1-j]);
+            }
+        }
     }
 };

98161475ca4c8163b4a3f421e9fe8ff1f02ac998
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-06 06:49:12 +0530

    Create 1502.cpp

---
+++ 1502.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) 
+    {
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<=arr.size()-2;i++)
+        {
+            if(arr[i]-arr[i-1]!=arr[i+1]-arr[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+};

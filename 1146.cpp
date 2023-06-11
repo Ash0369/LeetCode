9b4b3e1dcc6849b9315710e7d565d5b8eebcfee8
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-11 12:01:00 +0530

    Update 1146.cpp

--- 1146.cpp
+++ 1146.cpp
@@ -1,1 +1,38 @@
+class SnapshotArray 
+{
+public:
+    unordered_map<int,vector<int>>mp;
+    int id;
+    vector<map<int,int>>vec;
+    SnapshotArray(int length) 
+    {
+        vec.resize(length);
+        for(int i=0;i<length;i++)
+        {
+            vec[i][0]=0;
+        }
+        id=0;
+    }
+    
+    void set(int index, int val) 
+    {
+        vec[index][id]=val;
+    }
+    
+    int snap() 
+    {
+        id++;
+        return id-1;
+    }
+    
+    int get(int index, int snap_id) 
+    {
+        if(vec[index].find(snap_id)==vec[index].end()) 
+            {
+            auto it =--vec[index].lower_bound(snap_id);
+            return it->second;
 
+            }
+        return vec[index][snap_id];
+    }
+};

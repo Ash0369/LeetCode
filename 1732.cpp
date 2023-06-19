366c8b8dc3b33e5b45bb9ab79e5b8510f475e0b2
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-19 07:21:32 +0530

    Create 1732.cpp

---
+++ 1732.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int largestAltitude(vector<int>& gain) 
+    {
+        int ans=0;
+        int curr=0;
+        for(auto x:gain)
+        {
+            curr+=x;
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};

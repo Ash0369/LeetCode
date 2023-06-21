6bd8fecc5c860c2cab314c49e291963669ebf71a
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-06-21 06:42:38 +0530

    Create 2740.cpp

---
+++ 2740.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int findValueOfPartition(vector<int>& nums)
+    {
+        int n=nums.size();
+        int ans=1e9;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++)
+        {
+            ans=min(ans,nums[i]-nums[i-1]);
+        }
+        return ans;
+    }
+};

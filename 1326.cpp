eaa48aa166debf724f5b27530a6d571bde98c144
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-08-31 07:18:37 +0530

    Update 1326.cpp

--- 1326.cpp
+++ 1326.cpp
@@ -1,3 +1,5 @@
+//Method-1 : 
+
 class Solution 
 {
 public:
@@ -35,6 +37,30 @@ public:
                     }
                 }
             }
+        }
+        if(dp[n]>n)
+        {
+            return -1;
+        }
+        return dp[n];
+    }
+};
+
+//Method-2 : 
+
+class Solution 
+{
+public:
+    int minTaps(int n, vector<int>& ranges) 
+    {
+        vector<int>dp(n+2,n+10);
+        dp[0]=0;
+        for(int i=0;i<=n;i++)
+        {
+           for(int j=max(0,i-ranges[i]);j<=min(n,i+ranges[i]);j++)
+           {
+               dp[j]=min(dp[j],1+dp[max(0,i-ranges[i])]);
+           }
         }
         if(dp[n]>n)
         {

16d58959c5083c7f4239d2ca7dbb6c4587b1e260
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-09-26 06:47:50 +0530

    Create 316.cpp

---
+++ 316.cpp
@@ -0,0 +1,42 @@
+class Solution 
+{
+public:
+    string removeDuplicateLetters(string s) 
+    {
+        int n=s.size();
+        stack<int>st;
+        vector<vector<int>>dp(n+1,vector<int>(26,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<26;j++)
+            {
+                dp[i][j]=dp[i+1][j];
+            }
+            dp[i][s[i]-'a']++;
+        }
+        vector<int>dp1(26,0);
+        for(int i=0;i<n;i++)
+        {
+            if(dp1[s[i]-'a']==1)
+            {
+                continue;
+            }
+            while(!st.empty() && dp[i+1][st.top()]>0 && ((s[i]-'a')<st.top()))
+            {
+                dp1[st.top()]=0;
+                st.pop();
+            }
+            dp1[s[i]-'a']=1;
+            st.push(s[i]-'a');
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans.push_back(st.top()+'a');
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};

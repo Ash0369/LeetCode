816e6f6ecff462194bbf83fefd90fcbe21365c38
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-10-31 07:19:38 +0530

    Create 2433.cpp

---
+++ 2433.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    vector<int> findArray(vector<int>& pref) 
+    {
+        int n=pref.size();
+        vector<int>vec(n);
+        vec[0]=pref[0];
+        for(int i=1;i<n;i++)
+        {
+            vec[i]=pref[i-1]^pref[i];
+        }
+        return vec;
+    }
+};

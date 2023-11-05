1ca6b36dbd6b6d2aea2ec4349f614a216c8c3c07
ASHISH MOHANKA <89011500+Ash0369@users.noreply.github.com>
2023-11-05 07:00:11 +0530

    Update 1535.cpp

--- 1535.cpp
+++ 1535.cpp
@@ -4,14 +4,12 @@ public:
     int getWinner(vector<int>& arr, int k) 
     {
         int n=arr.size();
-        set<int>st;
         int x=arr[0];
         int cnt=0;
         for(int i=1;i<n;i++)
         {
             if(arr[i]>x)
             {
-                st.insert(x);
                 x=arr[i];
                 cnt=1;
                 if(cnt==k || cnt==n)
@@ -21,7 +19,6 @@ public:
             }
             else
             {
-                st.insert(arr[i]);
                 cnt++;
                 if(cnt==k || cnt==n)
                 {
@@ -29,23 +26,6 @@ public:
                 }
             }
         }
-        while(!st.empty())
-        {
-            if((*st.begin())<x)
-            {
-                cnt++; 
-                if(cnt==k || cnt==n)
-                {
-                    return x;
-                }
-            }
-            else
-            {
-                x=*st.begin();
-                cnt=0;
-            }
-            st.erase(*st.begin());
-        }
-        return -1;
+        return x;
     }
 };
